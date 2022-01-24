718333ea5e6cd00adc247a46c06afbf94f586761
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2022-01-24 09:35:02 +0000

    run pre-commits

--- src/transferscript/server/main.py
+++ src/transferscript/server/main.py
@@ -1,27 +1,33 @@
-from fastapi import FastAPI, Request, Response
-from pydantic import BaseModel
-import ispyb
+from __future__ import annotations
+
+import datetime
+import os
+import socket
 from pprint import pprint
+from typing import List, Optional
+
+import ispyb
 import sqlalchemy.exc
 import sqlalchemy.orm
+from fastapi import FastAPI, Request, Response
 from ispyb.sqlalchemy import BLSession, Proposal
-from typing import List, Optional
+from pydantic import BaseModel
 from requests import get
-import datetime
-import socket
-import os
 
 app = FastAPI()
 
 db_session = sqlalchemy.orm.sessionmaker(
     bind=sqlalchemy.create_engine(
         ispyb.sqlalchemy.url(), connect_args={"use_pure": True}
-    ))()
+    )
+)()
+
 
 @app.get("/")
 async def root(request: Request):
     client_host = request.client.host
     return {"client_host": client_host, "message": "Transfer Server"}
+
 
 class Visits(BaseModel):
     start: str
@@ -30,42 +36,87 @@ class Visits(BaseModel):
     visit_name: str
     proposal_title: str
 
+
 @app.get("/visits/{bl_name}")
 async def visit_info(bl_name: str):
-    query = db_session.query(BLSession).join(Proposal).filter(BLSession.proposalId == Proposal.proposalId, BLSession.beamLineName == bl_name, BLSession.endDate > datetime.datetime.now(), BLSession.startDate < datetime.datetime.now()).add_columns(BLSession.startDate, BLSession.endDate, BLSession.beamLineName, Proposal.proposalCode, Proposal.proposalNumber, BLSession.visit_number, Proposal.title).all()
+    query = (
+        db_session.query(BLSession)
+        .join(Proposal)
+        .filter(
+            BLSession.proposalId == Proposal.proposalId,
+            BLSession.beamLineName == bl_name,
+            BLSession.endDate > datetime.datetime.now(),
+            BLSession.startDate < datetime.datetime.now(),
+        )
+        .add_columns(
+            BLSession.startDate,
+            BLSession.endDate,
+            BLSession.beamLineName,
+            Proposal.proposalCode,
+            Proposal.proposalNumber,
+            BLSession.visit_number,
+            Proposal.title,
+        )
+        .all()
+    )
     if query:
-        return_query = [{"Start date": id.startDate, "End date": id.endDate, "Beamline name": id.beamLineName, "Visit name": id.proposalCode + str(id.proposalNumber) + '-' + str(id.visit_number), "Time remaining": str(id.endDate - datetime.datetime.now())} for id in query] # "Proposal title": id.title
+        return_query = [
+            {
+                "Start date": id.startDate,
+                "End date": id.endDate,
+                "Beamline name": id.beamLineName,
+                "Visit name": id.proposalCode
+                + str(id.proposalNumber)
+                + "-"
+                + str(id.visit_number),
+                "Time remaining": str(id.endDate - datetime.datetime.now()),
+            }
+            for id in query
+        ]  # "Proposal title": id.title
         return return_query
     else:
         return None
+
 
 @app.get("/pypi/{path}")
 async def pypi_request(path: str):
     full_path = "https://pypi.org/simple/" + path
     full_path_response = get(full_path)
-    return Response(content=full_path_response.content, media_type=full_path_response.headers['Content-Type'], status_code=full_path_response.status_code)
+    return Response(
+        content=full_path_response.content,
+        media_type=full_path_response.headers["Content-Type"],
+        status_code=full_path_response.status_code,
+    )
+
 
 @app.get("/pypi/")
 async def pypi_request():
     full_path = "https://pypi.org/simple/"
     full_path_response = get(full_path)
-    return Response(content=full_path_response.content, media_type=full_path_response.headers['Content-Type'], status_code=200)
+    return Response(
+        content=full_path_response.content,
+        media_type=full_path_response.headers["Content-Type"],
+        status_code=200,
+    )
+
 
 class File(BaseModel):
     name: str
     description: str
     size: int
 
+
 @app.post("/files", response_model=File)
 async def add_file(file: File):
     return file
+
 
 @app.get("/microscope")
 async def get_microscope():
     try:
         hostname = socket.gethostname()
-        microscope_from_hostname = hostname.split('.')[0]
+        microscope_from_hostname = hostname.split(".")[0]
     except OSError:
-        microscope_from_hostname = 'Unknown'
+        microscope_from_hostname = "Unknown"
     microscope_name = os.getenv("BEAMLINE", microscope_from_hostname)
     return microscope_name
--- src/transferscript/server/main_test.py
+++ src/transferscript/server/main_test.py
@@ -1,36 +1,45 @@
-from fastapi.testclient import TestClient
+from __future__ import annotations
+
 import json
 
+from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
+
 
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Transfer Server"}
 
+
 def test_get_visits():
     response = client.get("/visits/m12")
     assert response.status_code == 200
-    #assert response.json()[0]["Start date"] == "2020-09-09T14:00:00"
+    # assert response.json()[0]["Start date"] == "2020-09-09T14:00:00"
+
 
 def test_client_hostname():
     response = client.get("/")
     assert response.status_code == 200
 
+
 def test_pypi_proxy():
     response = client.get("/pypi/fastapi")
     assert response.status_code == 200
+
 
 def test_get_microscope():
     response = client.get("/microscope")
     assert response.status_code == 200
     print(response.content)
 
+
 def test_no_response():
     response = client.get("/hstnnsv")
     assert response.status_code != 200
+
 
 test_no_response()
 test_get_microscope()
