9ae86ffe709b16a360e2552821e093b62d37a7bf
Anna Horstmann <anna.horstmann@diamond.ac.uk>
2022-02-01 10:12:52 +0000

    Add logconfig.

--- src/transferscript/server/run.py
+++ src/transferscript/server/run.py
@@ -1,10 +1,23 @@
-import os
+from __future__ import annotations
+
 import argparse
-import uvicorn
+import logging
 import pathlib
 
+import uvicorn
+
+# from ... import logconfig
+
+
 def run():
+    log = logging.getLogger(name="Transferscript server")
+    # logconfig.setup_logging()
     parser = argparse.ArgumentParser(description="Start the transferscript server")
-    parser.add_argument("--env_file", help="Path to environment file", default=pathlib.Path(__file__).parent / 'example_environment_file')
+    parser.add_argument(
+        "--env_file",
+        help="Path to environment file",
+        default=pathlib.Path(__file__).parent / "example_environment_file",
+    )
     args = parser.parse_args()
+    log.info("Starting server (environment file: ", args.env_file, ")")
     uvicorn.run("server.main:app", host="127.0.0.1", port=8000, env_file=args.env_file)
