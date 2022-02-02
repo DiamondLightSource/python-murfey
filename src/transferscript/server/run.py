8c0c06f29ba7140c8b96a2ca99f95f3a2cce1bd9
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2022-02-02 09:49:48 +0000

    Fix server starting

--- src/transferscript/server/run.py
+++ src/transferscript/server/run.py
@@ -1,10 +1,22 @@
-import os
+from __future__ import annotations
+
 import argparse
-import uvicorn
 import pathlib
+
+import uvicorn
+
 
 def run():
     parser = argparse.ArgumentParser(description="Start the transferscript server")
-    parser.add_argument("--env_file", help="Path to environment file", default=pathlib.Path(__file__).parent / 'example_environment_file')
+    parser.add_argument(
+        "--env_file",
+        help="Path to environment file",
+        default=pathlib.Path(__file__).parent / "example_environment_file",
+    )
     args = parser.parse_args()
-    uvicorn.run("server.main:app", host="127.0.0.1", port=8000, env_file=args.env_file)
+    uvicorn.run(
+        "transferscript.server.main:app",
+        host="127.0.0.1",
+        port=8000,
+        env_file=args.env_file,
+    )
