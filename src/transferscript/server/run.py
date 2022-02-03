f7c9271a826ec284d360d59ef5cba3e3c15866f9
Anna Horstmann <67278233+HorstmannA@users.noreply.github.com>
2022-02-03 14:10:26 +0000

    Logging (#19)
    Use Zocalo logging config to log the server start-up to Graylog

--- src/transferscript/server/run.py
+++ src/transferscript/server/run.py
@@ -1,12 +1,22 @@
 from __future__ import annotations
 
 import argparse
+import logging
 import pathlib
 
 import uvicorn
+import zocalo.configuration
+
+ZOCALO_CONFIG = "/dls_sw/apps/zocalo/live/configuration.yaml"
 
 
 def run():
+    # setup logging
+    log = logging.getLogger(name="Transferscript server")
+    log.setLevel(logging.INFO)
+    zc = zocalo.configuration.from_file(ZOCALO_CONFIG)
+    zc.activate_environment("live")
+
     parser = argparse.ArgumentParser(description="Start the transferscript server")
     parser.add_argument(
         "--env_file",
@@ -14,9 +24,13 @@ def run():
         default=pathlib.Path(__file__).parent / "example_environment_file",
     )
     args = parser.parse_args()
+    print(args.env_file)
+    log.info("Starting transferscript server.")
     uvicorn.run(
         "transferscript.server.main:app",
         host="127.0.0.1",
         port=8000,
         env_file=args.env_file,
-    )
+        log_level="warning",
+    )  # set to warning to reduce log clogging
+    log.info("Server startup complete.")
