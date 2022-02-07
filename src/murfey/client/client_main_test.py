61d1880d2286d2aad0651300bf11fd8e687c934a
Markus Gerstel <2102431+Anthchirp@users.noreply.github.com>
2022-02-07 10:33:27 +0000

    Run tests on Azure (#26)
    This sets the minimum Python version to 3.8.
    Zocalo has already dropped 3.7,
    and it's probably sensible to follow NEP-29 here as well.
    
    Azure jobs run a dummy ISPyB database and a RabbitMQ server, but these are
    not yet configured for the tests, therefore those tests currently xfail.
    Client/server tests are also not yet supported on Azure.

--- src/murfey/client/client_main_test.py
+++ src/murfey/client/client_main_test.py
@@ -4,13 +4,13 @@ import os
 
 os.environ["BEAMLINE"] = "m12"
 print(os.environ["BEAMLINE"])
-import main
+import pytest
+
+import murfey.client.main as main
 
 
+@pytest.mark.xfail
 def test_get_visit_info():
     print("BEAMLINE", os.environ["BEAMLINE"])
     response = main.get_visit_info("cm31095-1")
     assert response.status_code == 200
-
-
-test_get_visit_info()
--- src/murfey/server/main_test.py
+++ src/murfey/server/main_test.py
@@ -1,40 +1,45 @@
 from __future__ import annotations
 
+import pytest
 from fastapi.testclient import TestClient
 
-from murfey.server.main import app
 
-client = TestClient(app)
+@pytest.fixture(scope="session")
+def client():
+    pytest.xfail("Will fail if server not running")
+    from murfey.server.main import app
+
+    return TestClient(app)
 
 
-def test_read_main():
+def test_read_main(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Transfer Server"}
 
 
-def test_get_visits():
+def test_get_visits(client):
     response = client.get("/visits/m12")
     assert response.status_code == 200
     # assert response.json()[0]["Start date"] == "2020-09-09T14:00:00"
 
 
-def test_client_hostname():
+def test_client_hostname(client):
     response = client.get("/")
     assert response.status_code == 200
 
 
-def test_pypi_proxy():
+def test_pypi_proxy(client):
     response = client.get("/pypi/fastapi")
     assert response.status_code == 200
 
 
-def test_get_microscope():
+def test_get_microscope(client):
     response = client.get("/microscope")
     assert response.status_code == 200
     print(response.content)
 
 
-def test_no_response():
+def test_no_response(client):
     response = client.get("/hstnnsv")
     assert response.status_code != 200
