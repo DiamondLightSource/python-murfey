6fc08fd551cf801ddfc185f3618be19f818b6389
Markus Gerstel <2102431+Anthchirp@users.noreply.github.com>
2022-02-14 09:11:31 +0000

    Enable server tests on Azure (#29)
    * Move server test into tests directory
    * Add missing dependencies in requirements file
    * Add ISPyB configuration file
    * Fix Python 3.8 import fallback

---
+++ tests/server/test_main.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from fastapi.testclient import TestClient
+
+from murfey.server.main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "<title>" in response.text.lower()
+
+
+def test_get_visits():
+    response = client.get("/visits/")
+    assert response.status_code == 200
+    # assert response.json()[0]["Start date"] == "2020-09-09T14:00:00"
+
+
+def test_pypi_proxy():
+    response = client.get("/pypi/fastapi")
+    assert response.status_code == 200
+    assert "<a href" in response.text.lower()
+    assert ".tar.gz" in response.text.lower()
+
+
+def test_file_not_found_response():
+    response = client.get("/hstnnsv")
+    assert response.status_code == 404
