1a0ba902b77f2c27eeae19c185e5debfbddd05c2
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2022-03-15 14:14:31 +0000

    server.websocket: turn print() into logging

--- src/murfey/server/websocket.py
+++ src/murfey/server/websocket.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
 import asyncio
+import logging
 from typing import Dict
 
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 
 ws = APIRouter(prefix="/ws", tags=["websocket"])
+log = logging.getLogger("murfey.server.websocket")
 
 
 class ConnectionManager:
@@ -25,7 +27,7 @@ class ConnectionManager:
 
     async def broadcast(self, message: str):
         for connection in self.active_connections:
-            print(f"Sending '{message}'")
+            log.info(f"Sending '{message}'")
             await self.active_connections[connection].send_text(message)
 
 
@@ -42,24 +44,24 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
             data = await websocket.receive_text()
             await manager.broadcast(f"Client #{client_id} sent message {data}")
     except WebSocketDisconnect:
-        print(f"Disconnecting Client {client_id}")
+        log.info(f"Disconnecting Client {client_id}")
         manager.disconnect(websocket, client_id)
         await manager.broadcast(f"Client #{client_id} disconnected")
 
 
 async def check_connections(active_connections):
-    print("Checking connections")
+    log.info("Checking connections")
     for connection in active_connections:
-        print("Checking response")
+        log.info("Checking response")
         try:
             await asyncio.wait_for(connection.receive(), timeout=6)
         except asyncio.TimeoutError:
-            print(f"Disconnecting Client {connection[0]}")
+            log.info(f"Disconnecting Client {connection[0]}")
             manager.disconnect(connection[0], connection[1])
 
 
 @ws.delete("/ws/test/{client_id}")
 async def close_ws_connection(client_id):
-    print("Disconnecting", client_id)
+    log.info("Disconnecting", client_id)
     manager.disconnect(manager.active_connections[client_id], client_id)
     await manager.broadcast(f"Client #{client_id} disconnected")
