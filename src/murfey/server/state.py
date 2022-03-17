432614ace16fb3f64d18bfbdc9b3e132aa89bec6
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2022-03-17 07:31:03 +0000

    make state object a generic

--- src/murfey/server/state.py
+++ src/murfey/server/state.py
@@ -1,27 +1,27 @@
 from __future__ import annotations
 
 import collections
-from typing import Callable, Union
+from typing import Callable, TypeVar, Union
 
-AttributeValue = Union[str, int, None]
-UpdateFunction = Callable[[str, AttributeValue], None]
+T = TypeVar("T")
+GlobalStateValues = Union[str, int, None]
 
 
-class State(collections.UserDict[str, AttributeValue]):
+class State(collections.UserDict[str, T]):
     """A helper class to coordinate shared state across server instances.
     This is a dictionary implementing the Observer pattern and is mostly used
     as a singleton."""
 
     def __init__(self):
-        self._listeners: list[UpdateFunction] = []
+        self._listeners: list[Callable[[str, T | None], None]] = []
         super().__init__()
 
-    def __delitem__(self, key):
+    def __delitem__(self, key: str):
         super().__delitem__(key)
         for notify_function in self._listeners:
             notify_function(key, None)
 
-    def __setitem__(self, key: str, value: AttributeValue):
+    def __setitem__(self, key: str, value: T):
         super().__setitem__(key, value)
         for notify_function in self._listeners:
             notify_function(key, value)
@@ -29,8 +29,8 @@ class State(collections.UserDict[str, AttributeValue]):
     def __repr__(self):
         return f"{type(self).__name__}({self.data}; {len(self._listeners)} subscribers)"
 
-    def subscribe(self, fn: UpdateFunction):
+    def subscribe(self, fn: Callable[[str, T | None], None]):
         self._listeners.append(fn)
 
 
-state = State()
+global_state: State[GlobalStateValues] = State()
