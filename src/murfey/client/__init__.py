afccfcf5e75ab5e001b40fa21c7eeddd161fb32a
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2022-07-20 17:29:03 +0100

    Add way of using m12 as a demo without setting environment variable

--- src/murfey/client/__init__.py
+++ src/murfey/client/__init__.py
@@ -149,8 +149,6 @@ def run():
     pprint(ongoing_visits)
     ongoing_visits = [v.name for v in ongoing_visits]
 
-    ongoing_visits = ["cm31111-1"]
-
     _enable_webbrowser_in_cygwin()
 
     log.setLevel(logging.DEBUG)
