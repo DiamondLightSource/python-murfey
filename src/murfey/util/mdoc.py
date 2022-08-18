e6c9a0cc4a534961ec1fcb22f2103754f33769c0
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2022-08-18 11:21:03 +0100

    Add mdoc global data parsing

--- src/murfey/util/mdoc.py
+++ src/murfey/util/mdoc.py
@@ -1,6 +1,14 @@
 from __future__ import annotations
 
+from datetime import datetime
 from typing import TextIO
+
+
+def _basic_parse(line: str) -> dict:
+    kv = [p.strip() for p in line.split("=")]
+    if kv[0] == "DateTime":
+        return {kv[0]: datetime.strptime(kv[1], "%d-%b-%y %H:%M:%S")}
+    return {kv[0]: tuple(kv[1].split()) if " " in kv[1] else kv[1]}
 
 
 def get_block(mdocfile: TextIO) -> dict:
@@ -13,6 +21,14 @@ def get_block(mdocfile: TextIO) -> dict:
     while line := mdocfile.readline():
         if line.replace(" ", "") == "\n":
             break
-        kv = [p.strip() for p in line.split("=")]
-        as_dict[kv[0]] = tuple(kv[1].split()) if " " in kv[1] else kv[1]
+        as_dict.update(_basic_parse(line))
+    return as_dict
+
+
+def get_global_data(mdocfile: TextIO) -> dict:
+    as_dict = {}
+    while line := mdocfile.readline():
+        if line.startswith("[") or line.replace(" ", "") == "\n":
+            break
+        as_dict.update(_basic_parse(line))
     return as_dict
