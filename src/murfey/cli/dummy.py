c71345d4121d9bd80f1b9f8c6b092056eb7273e9
Dan Hatton <daniel.hatton@diamond.ac.uk>
2022-09-14 13:21:24 +0100

    Fixes for simulated data production

--- src/murfey/cli/dummy.py
+++ src/murfey/cli/dummy.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import argparse
 from pathlib import Path
 
-from murfey.util.dummy_setup import generate_detector_data, initialise
+from murfey.util.dummy_setup import generate_data, initialise
 
 
 def run():
@@ -21,7 +21,15 @@ def run():
         help="Number of seconds after which dummy data collection will finish",
         default=60,
     )
+    parser.add_argument(
+        "-p",
+        "--pause",
+        dest="pause",
+        type=int,
+        help="Number of seconds after which dummy data collection will finish",
+        default=10,
+    )
     args = parser.parse_args()
 
     base_path = initialise(Path(args.dummy_dir))
-    generate_detector_data(base_path, timeout=args.timeout)
+    generate_data(base_path, timeout=args.timeout, pause=args.pause)
--- src/murfey/util/dummy_setup.py
+++ src/murfey/util/dummy_setup.py
@@ -7,9 +7,11 @@ from pathlib import Path
 import mrcfile
 import numpy as np
 import xmltodict
+import yaml
+from rich.prompt import Confirm
 
 TILT = 1
-tilt_angle = itertools.cycle(range(-60, 60, 10))
+tilt_angle = itertools.cycle(range(-60, 70, 10))
 
 
 def initialise(dummy_location: Path) -> Path:
@@ -18,22 +20,47 @@ def initialise(dummy_location: Path) -> Path:
     microscope_dir = base / "M"
     detector_dir = base / "Data"
     microscope_dir.mkdir()
+    (microscope_dir / "Supervisor").mkdir()
     detector_dir.mkdir()
     (detector_dir / "Supervisor").mkdir()
+    with open(base / "config.yaml", "w") as yaml_out:
+        yaml.dump(
+            {
+                "m12": {
+                    "aqcuisition_software": ["epu", "tomo", "serialem"],
+                    "data_directories": {
+                        str(detector_dir): "detector",
+                        str(microscope_dir): "microscope",
+                    },
+                    "calibrations": {"dummy": 0},
+                }
+            },
+            yaml_out,
+        )
+        print(
+            f"To set Murfey configuration for server: export MURFEY_MACHINE_CONFIGURATION={base / 'config.yaml'}"
+        )
     return base
 
 
 def tomo_file_name() -> str:
     angle = next(tilt_angle)
     global TILT
-    if angle == -60 and TILT != 1:
+    file_name = f"Position_{TILT}_[{angle}].mrc"
+    if angle == 60:
         TILT += 1
-    return f"Position_{TILT}_[{next(tilt_angle)}].mrc"
+    return file_name
 
 
-def write_mrc(base_path: Path, session_dir: str, mrc_name: str):
-    rand_data = np.random.randint(0, high=128, size=(2048, 2048, 50), dtype=np.uint8)
-    with mrcfile.new(base_path / "Data" / session_dir / mrc_name) as mrc:
+def write_mrc(
+    base_path: Path,
+    session_dir: str,
+    mrc_name: str,
+    data_dir: str = "Data",
+    size: tuple = (1024, 1024, 50),
+):
+    rand_data = np.random.randint(0, high=128, size=size, dtype=np.uint8)
+    with mrcfile.new(base_path / data_dir / session_dir / mrc_name) as mrc:
         mrc.set_data(rand_data)
 
 
@@ -56,16 +83,28 @@ def write_xml(base_path: Path, session_dir: str, xml_name: str):
         xml.write(xmltodict.unparse(xml_data))
 
 
-def generate_detector_data(base_path: Path, timeout: int | None = None):
-    start_time = time.time()
-    while True:
-        try:
-            if timeout is not None:
-                if time.time() - start_time > timeout:
-                    return
-            tfn = tomo_file_name()
-            write_mrc(base_path, "Supervisor", tfn)
-            write_xml(base_path, "Supervisor", tfn.replace(".mrc", ".xml"))
-            time.sleep(10)
-        except KeyboardInterrupt:
-            return
+def generate_data(base_path: Path, timeout: int | None = None, pause: int = 10):
+    if Confirm.ask("Begin simulated data acquisiton?"):
+        start_time = time.time()
+        while True:
+            try:
+                if timeout is not None:
+                    if time.time() - start_time > timeout:
+                        return
+                tilt = TILT
+                tfn = tomo_file_name()
+                try:
+                    write_mrc(
+                        base_path,
+                        "Supervisor",
+                        f"Position_{tilt}.mrc",
+                        data_dir="M",
+                        size=(128, 128),
+                    )
+                except ValueError:
+                    pass
+                write_mrc(base_path, "Supervisor", tfn)
+                write_xml(base_path, "Supervisor", tfn.replace(".mrc", ".xml"))
+                time.sleep(pause)
+            except KeyboardInterrupt:
+                return
