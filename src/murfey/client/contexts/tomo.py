0f0fe3aa9914040fc1246994884ea75d13537a52
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2022-11-07 10:33:22 +0000

    Start using version based extraction functions for Tomo

--- src/murfey/client/contexts/tomo.py
+++ src/murfey/client/contexts/tomo.py
@@ -22,8 +22,41 @@ def _get_tilt_tag_v5_8(p: Path) -> str:
     return p.name.split("_")[0]
 
 
+def _get_slice_index_v5_10(tag: str) -> int:
+    slice_index = 0
+    for i, ch in enumerate(tag[::-1]):
+        if not ch.isnumeric():
+            slice_index = -i
+            break
+    if not slice_index:
+        raise ValueError(
+            f"The file tag {tag} does not end in numeric characters or is entirely numeric: cannot parse"
+        )
+    return slice_index
+
+
+def _get_tilt_series_v5_10(p: Path) -> str:
+    tag = p.name.split("_")[0]
+    slice_index = _get_slice_index_v5_10(tag)
+    return tag[slice_index:]
+
+
+def _get_tilt_angle_v5_10(p: Path) -> str:
+    tag = p.name.split("_")[0]
+    slice_index = _get_slice_index_v5_10(tag)
+    return tag[:slice_index]
+
+
+def _get_tilt_tag_v5_10(p: Path) -> str:
+    _split = p.name.split("_")[2].split(".")
+    return ".".join(_split[:-1])
+
+
 tomo_tilt_info = {
     "5.8": TiltInfoExtraction(
         _get_tilt_series_v5_8, _get_tilt_angle_v5_8, _get_tilt_tag_v5_8
-    )
+    ),
+    "5.10": TiltInfoExtraction(
+        _get_tilt_series_v5_10, _get_tilt_angle_v5_10, _get_tilt_tag_v5_10
+    ),
 }
