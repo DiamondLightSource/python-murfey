dd012af08a0aec8dae0e12987758147e395cb93b
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2022-11-07 12:42:37 +0000

    Minor fix

--- src/murfey/client/__init__.py
+++ src/murfey/client/__init__.py
@@ -270,7 +270,7 @@ def run():
 
     instance_environment = MurfeyInstanceEnvironment(
         url=murfey_url,
-        software_versions=machine_data.software_versions,
+        software_versions=machine_data.get("software_versions", {}),
         source=Path(args.source),
         watcher=source_watcher,
         default_destination=args.destination
