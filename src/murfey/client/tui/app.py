78721f5d997121dafec956bbfd569ac0d58ba770
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2022-12-12 13:41:36 +0000

    If rsyncing gain reference file then process it as well

--- src/murfey/client/tui/app.py
+++ src/murfey/client/tui/app.py
@@ -548,7 +548,7 @@ class MurfeyTUI(App):
                 gain_rsync = procrunner.run(
                     [
                         "rsync",
-                        str(self._enviornment.gain_ref),
+                        str(self._environment.gain_ref),
                         f"{self._url.hostname}::{visit_path}/processing",
                     ]
                 )
@@ -556,6 +556,12 @@ class MurfeyTUI(App):
                     log.warning(
                         f"Gain reference file {self._environment.gain_ref} was not successfully transferred to {visit_path}/processing"
                     )
+                else:
+                    url = f"{str(self._url.geturl())}/visits/{str(self._visit)}/process_gain"
+                    post_response = requests.post(
+                        url, {"gain_ref": self._environment.gain_ref}
+                    )
+                    self._environment.gain_ref = post_response.json().get("gain_ref")
         if not self.rsync_process:
             self.rsync_process = RSyncer(
                 self._source,
