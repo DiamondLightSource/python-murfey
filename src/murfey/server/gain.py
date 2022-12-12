c0519ca9926028736467e82ced7878d92dfef20d
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2022-12-12 13:18:51 +0000

    Add prepare_gain method to use imod executables to prepare the dm4 gain reference

---
+++ src/murfey/server/gain.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+import asyncio
+from enum import Enum
+from pathlib import Path
+from typing import Dict
+
+
+class Camera(Enum):
+    K3_FLIPX = 1
+    K3_FLIPY = 2  # Talos
+    FALCON = 3
+
+
+async def prepare_gain(
+    camera: int, gain_path: Path, executables: Dict[str, str]
+) -> bool:
+    if camera == Camera.FALCON:
+        return True
+    if gain_path.suffix == ".dm4":
+        flip = "flipx" if camera == Camera.K3_FLIPX else "flipy"
+        dm4_proc = await asyncio.create_subprocess_shell(
+            f"{executables['dm2mrc']} {gain_path} {gain_path.with_suffix('.mrc')}",
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout, stderr = await dm4_proc.communicate()
+        if dm4_proc.returncode:
+            return False
+        clip_proc = await asyncio.create_subprocess_shell(
+            f"{executables['clip']} {flip} {gain_path.with_suffix('.mrc')} {gain_path.parent / (gain_path.name+'_superres.mrc')}",
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout, stderr = await clip_proc.communicate()
+        if clip_proc.returncode:
+            return False
+        newstack_proc = await asyncio.create_subprocess_shell(
+            f"{executables['newstack']} {flip} {gain_path.with_suffix('.mrc')} {gain_path.parent / (gain_path.name+'_superres.mrc')}",
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout, stderr = await newstack_proc.communicate()
+        if newstack_proc.returncode:
+            return False
+        return True
+    return False
