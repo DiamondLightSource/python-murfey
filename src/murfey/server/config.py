c9e78706bd765f4feff7a0efc72d07ba3c5d9738
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2023-04-16 21:05:31 +0100

    Enable removing source files from machine configuration (#169)

--- src/murfey/server/config.py
+++ src/murfey/server/config.py
@@ -21,6 +21,7 @@ class MachineConfig(BaseModel):
     superres: bool = False
     camera: str = "FALCON"
     data_required_substrings: Dict[str, List[str]] = {}
+    allow_removal: bool = False
 
 
 def from_file(config_file_path: Path, microscope: str) -> MachineConfig:
