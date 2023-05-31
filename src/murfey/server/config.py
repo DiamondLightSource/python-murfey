f5932100ff0fa58abb5b43e7e57a1ab5585e4e8d
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2023-05-31 20:26:04 +0100

    Fix pydantic optional use

--- src/murfey/server/config.py
+++ src/murfey/server/config.py
@@ -17,7 +17,7 @@ class MachineConfig(BaseModel):
     rsync_basepath: Path
     murfey_db_credentials: str
     display_name: str = ""
-    image_path: Path | None = None
+    image_path: Optional[Path] = None
     software_versions: Dict[str, str] = {}
     external_executables: Dict[str, str] = {}
     rsync_module: str = ""
