4cede9ee9b84a3b7ff8bc579d4e49bc57f9283e0
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2023-08-23 15:41:09 +0100

    Typo

--- src/murfey/client/contexts/tomo.py
+++ src/murfey/client/contexts/tomo.py
@@ -925,7 +925,7 @@ class TomographyContext(Context):
                     metadata_file.parent / data_file
                 )
         except Exception as e:
-            logger.warning(f"Eception encountered in metadata gathering: {str(e)}")
+            logger.warning(f"Exception encountered in metadata gathering: {str(e)}")
             return OrderedDict({})
 
         return mdoc_metadata
