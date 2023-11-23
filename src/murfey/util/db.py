03c13fe68477e3e0940aa62f46059154022b066a
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2023-11-23 12:51:16 +0000

    Make EER fractionation file column nullable as one often doesn't exist

--- src/murfey/util/db.py
+++ src/murfey/util/db.py
@@ -134,7 +134,7 @@ class PreprocessStash(SQLModel, table=True):  # type: ignore
     client_id: int = Field(primary_key=True, foreign_key="clientenvironment.client_id")
     image_number: int
     mrc_out: str
-    eer_fractionation_file: str
+    eer_fractionation_file: Optional[str]
     client: Optional[ClientEnvironment] = Relationship(
         back_populates="preprocess_stashes"
     )
