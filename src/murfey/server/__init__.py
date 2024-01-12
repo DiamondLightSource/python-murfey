e803a69dc5d5c16f35d4133ecfa09442f2f36cef
stephen-riggs <stephen.riggs@diamond.ac.uk>
2024-01-12 14:25:57 +0000

    Remove duplicate data collection group insert

--- src/murfey/server/__init__.py
+++ src/murfey/server/__init__.py
@@ -1818,26 +1818,13 @@ def feedback_callback(header: dict, message: dict) -> None:
                         message.get("tag"): dcgid
                     }
                 _transport_object.transport.ack(header)
-            client = murfey_db.exec(
-                select(db.ClientEnvironment).where(
-                    db.ClientEnvironment.client_id == message["client_id"]
-                )
-            ).one()
-            murfey_dcg = db.DataCollectionGroup(
-                id=dcgid,
-                session_id=client.session_id,
-                tag=message.get("tag"),
-            )
-            murfey_db.add(murfey_dcg)
-            murfey_db.commit()
-            murfey_db.close()
             return None
         elif message["register"] == "data_collection":
             dcg = murfey_db.exec(
                 select(db.DataCollectionGroup).where(
                     db.DataCollectionGroup.tag == message["source"]
                 )
-            ).all()
+            ).one()
             if dcg:
                 dcgid = dcg.id
                 # flush_data_collections(message["source"], murfey_db)
