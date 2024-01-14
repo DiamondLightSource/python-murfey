bcf9575b079ce1ab29efdfe30bcd96d9b1aa12d2
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2024-01-14 23:47:51 +0000

    Change order to avoid race condition

--- src/murfey/client/tui/app.py
+++ src/murfey/client/tui/app.py
@@ -470,11 +470,11 @@ class MurfeyTUI(App):
                 f"{self.app._environment.url.geturl()}/clients/{self.app._environment.client_id}/tomography_preprocessing_parameters",
                 json=json,
             )
+            context._flush_data_collections()
             capture_post(
                 f"{self.app._environment.url.geturl()}/visits/{self._visit}/{self.app._environment.client_id}/flush_tomography_processing",
                 json={"rsync_source": str(source)},
             )
-            context._flush_data_collections()
         elif isinstance(context, SPAContext) or isinstance(context, SPAModularContext):
             url = f"{str(self._url.geturl())}/visits/{str(self._visit)}/{self._environment.client_id}/register_data_collection_group"
             dcg_data = {
