bb5370f144167cf532d2fa3c35cd50c8342e9227
Eu Pin Tien <wjr38212@ws466.diamond.ac.uk>
2024-04-04 14:03:36 +0100

    Included explanation (with source) for why setup() is to be kept as-is.

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-#!/usr/bin/env python
-
 from __future__ import annotations
-
-import setuptools
+from setuptools import setup
 
 if __name__ == "__main__":
+    """
+    If compatibility with legacy builds (i.e. those not using the PEP 517
+    build API) is desired, a setup.py file containing a setup() function
+    call is still required even if your configuration resides in setup.cfg.
+    REF: https://setuptools.pypa.io/en/latest/userguide/declarative_config.html
+    """
     # Do not add any parameters here. Edit setup.cfg instead.
-    setuptools.setup()
+    setup()
