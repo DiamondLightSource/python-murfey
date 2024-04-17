f3d6856b0cd3b245fbe177dcb95329773a67de4e
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2024-04-17 17:45:15 +0100

    Added placeholder function to lif.py

--- src/murfey/util/lif.py
+++ src/murfey/util/lif.py
@@ -3,3 +3,7 @@ This script contains functions to that help with reading .lif files and converti
 """
 
 from __future__ import annotations
+
+def open_lif_file():
+    # Code block here
+    return None
