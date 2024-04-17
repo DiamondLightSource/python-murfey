e1f637eee2015647a70aa51c3cef6981308eaed5
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2024-04-17 16:51:55 +0100

    Created utility Python script to process .lif files for cryo-CLEM workflow.

---
+++ src/murfey/util/lif.py
@@ -0,0 +1,5 @@
+"""
+This script contains functions to that help with reading .lif files and converting them into other useful file formats (e.g. .tiff files)
+"""
+
+from __future__ import annotations
