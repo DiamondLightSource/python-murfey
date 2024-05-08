d31ee967da2aa21346c62c5a3e6de5441641fdbc
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2024-05-08 16:34:37 +0100

    Added files needed for Murfey to process cryo-CLEM data

---
+++ src/murfey/client/contexts/clem.py
@@ -0,0 +1,7 @@
+"""
+Add a CLEM class context (refer to src/murfey/client/contexts/fib.py)
+
+Provide a post transfer function to pass on:
+- File path on the DLS file system
+- Session ID (src/murfey/client/instance_environment.py)
+"""
