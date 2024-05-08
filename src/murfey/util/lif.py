57dfba92f54d109beb50fe6003ce2ba9e724bd5d
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2024-05-08 15:32:05 +0100

    Updated descriptions of functions and tidied up comments

--- src/murfey/util/lif.py
+++ src/murfey/util/lif.py
@@ -1,6 +1,6 @@
 """
-Contains functions that help with reading .lif files and converting them into other
-useful file formats (e.g. .tiff files) as part of the cryo-CLEM workflow
+Contains functions that help with reading LIF files and converting them into TIFF files
+as part of the cryo-CLEM workflow.
 """
 
 from __future__ import annotations
@@ -24,16 +24,18 @@ def get_xml_metadata(
     Extracts and returns the file metadata as a formatted XML tree, and optionally
     saves it as an XML file to the specified file path.
     """
-    xml_root = file.xml_root  # This one for navigating
+
+    # Use readlif function to get XML metadata
+    xml_root: ET.Element = file.xml_root  # This one for navigating
     xml_tree = ET.ElementTree(xml_root)  # This one for saving
 
+    # Skip saving the metadata if save_xml not provided
     if not save_xml:
-        pass  # Skip saving the metadata
+        pass
     else:
-        xml_file = str(save_xml)
-        # Save XML metadata for LIF file as one single unit
-        ET.indent(xml_tree, "  ")  # Tidy it up
-        xml_tree.write(xml_file, encoding="utf-8")
+        xml_file = str(save_xml)  # Convert Path to string
+        ET.indent(xml_tree, "  ")  # Format with proper indentation
+        xml_tree.write(xml_file, encoding="utf-8")  # Save
         print(f"File metadata saved to {xml_file}")
         pass
 
@@ -42,14 +44,16 @@ def get_xml_metadata(
 
 def get_image_elements(root: ET.Element) -> List[ET.Element]:
     """
-    Searches through the XML metadata recursively to find the nodes tagged as "Element"
-    that have image-related tags. Some LIF datasets have layers of nested elements.
+    Searches the XML metadata recursively to find the nodes tagged as "Element" that
+    have image-related tags. Some LIF datasets have layers of nested elements, so a
+    recursive approach is needed to avoid certain datasets breaking it.
     """
 
-    # Nested function which generates list of elements with
+    # Nested function which generates list of elements
     def _find_elements_recursively(
         node: ET.Element,
     ) -> Generator[ET.Element, None, None]:
+
         # Find items labelled "Element" under current node
         elem_list = node.findall("./Children/Element")
         if len(elem_list) < 1:  # Try alternative path for top-level of XML tree
@@ -58,21 +62,26 @@ def get_image_elements(root: ET.Element) -> List[ET.Element]:
         # Recursively search for items tagged as Element under child branches
         for elem in elem_list:
             yield elem
-            new_node = elem
-            new_elem_list = _find_elements_recursively(new_node)
+            new_node = elem  # New starting point for the search
+            new_elem_list = _find_elements_recursively(new_node)  # Call self
             for new_elem in new_elem_list:
                 yield new_elem
 
     # Get initial list of elements
     elem_list = list(_find_elements_recursively(root))
 
-    # Keep only the ones that have image-related tags
+    # Keep only the element nodes that have image-related tags
     elem_list = [elem for elem in elem_list if elem.find("./Data/Image")]
 
     return elem_list
 
 
 def raise_bit_depth_error(bit_depth: int):
+    """
+    Raises an exception if the bit depth value provided is not one that NumPy can
+    handle.
+    """
+
     raise Exception(
         "The channel bit depth provided is not compatible with Numpy. "
         "Only 8, 16, 32, and 64-bit channel depths are allowed. "
@@ -87,6 +96,7 @@ def change_bit_depth(
     """
     Change the bit depth of the array without changing the values (barring rounding).
     """
+
     # Use shorter terms
     arr = array
 
@@ -111,9 +121,10 @@ def rescale_across_channel(
     round_to: Optional[int] = 16,  # Round bounds to reasonably granular power of 2
 ) -> np.ndarray:
     """
-    Checks the range of values occupied by the data, then rescales it across the
+    Checks the range of pixel values occupied by the data, then rescales it across the
     channel's bit depth.
     """
+
     # Check that bit depth is valid before processing even begins
     if not any(bit_depth == b for b in [8, 16, 32, 64]):
         raise_bit_depth_error(bit_depth)
@@ -122,6 +133,8 @@ def rescale_across_channel(
 
     # Use shorter variable names
     arr = array
+
+    # Check if percentiles are provided
     if not percentile_range:
         print("No percentile range provided. Returning original array.")
         pass
@@ -132,7 +145,9 @@ def rescale_across_channel(
 
         # Calculate lower and upper bounds
         b_lo = np.floor(np.percentile(arr, p_lo) / round_to) * round_to  # Lower bound
-        b_up = np.ceil(np.percentile(arr, p_up) / round_to) * round_to  # Upper bound
+        b_up = (
+            np.ceil(np.percentile(arr, p_up) / round_to) * round_to
+        ) - 1  # Upper bound
 
         # Rescale across channel bit depth
         arr[arr < b_lo] = b_lo  # Overwrite lower outliers
@@ -140,12 +155,13 @@ def rescale_across_channel(
         arr = arr - b_lo  # Shift lower bound to zero
         arr = (arr / (b_up - b_lo)) * (
             2**bit_depth - 1
-        )  # Ensure data points don't exceed bit depth
+        )  # Ensure data points don't exceed bit depth (max bit is 2**n - 1)
 
         # This step probably not needed
+        # Overwrite values that exceed current channel bit depth
         # arr[arr >= (2**bit_depth - 1)] = (
         #     2**bit_depth - 1
-        # )  # Ensure data points don't exceed bit depth
+        # )
 
         # Change bit depth back to initial one
         arr = change_bit_depth(arr, bit_depth)
@@ -158,23 +174,28 @@ def rescale_to_bit_depth(
     initial_bit_depth: int,
     target_bit_depth: int,
 ) -> Tuple[np.ndarray, int]:
+    """
+    Rescales the pixel values of the array to fit within the desired channel bit depth.
+    Returns the array and the target bit depth as a tuple.
+    """
 
     # Use shorter names for variables
     arr = array
     bit_init = initial_bit_depth
     bit_final = target_bit_depth
 
-    # Check that allowed target bit depth is given
+    # Check that target bit depth is allowed
     if not any(bit_final == b for b in [8, 16, 32, 64]):
         raise_bit_depth_error(bit_final)
     else:
-        pass  # Continue with rest of function
+        pass  # Carry on
 
     # Rescale (DIVIDE BEFORE MULTIPLY)
-    arr = (arr / (2**bit_init - 1)) * (2**bit_final - 1)  # Avoid exceeding bit depth
+    arr = (arr / (2**bit_init - 1)) * (2**bit_final - 1)
 
-    # This step probably not needed
-    # arr[arr >= (2**bit_final - 1)] = 2**bit_final - 1  # Overwrite pixels that exceed channel bit depth
+    # This step probably not needed anymore
+    # Overwrite pixels that exceed channel bit depth
+    # arr[arr >= (2**bit_final - 1)] = 2**bit_final - 1
 
     # Change to correct unsigned integer type
     arr = change_bit_depth(arr, bit_final)
@@ -184,62 +205,74 @@ def rescale_to_bit_depth(
 
 def convert_lif_to_tiff(file: Path):
     """
-    DLS eBIC experiment sessions have the following folder structure:
-    parent          <- Session ID
+    Takes a LIF file, extracts its metadata as an XML tree, then parses through the
+    sub-images stored inside it, saving each channel in the sub-image as a separate
+    image stack. It uses information stored in the XML metadata to name the individual
+    image stacks.
+
+    FOLDER STRUCTURE
+    ================
+    Here is the folder structure of a typical DLS eBIC experiment session, with the
+    folders created as part of the workflow shown as well.
+
+    parent_folder   <- Session ID
     |_ processing   <- ARCHIVED BY DLS; Not used
-    |_ spool        <- NOT ARCHIVED BY DLS; For work proprietary work
-    |_ tmp          <- DELETED; Intermediate files
+    |_ spool        <- NOT ARCHIVED BY DLS; For confidential work
+    |_ tmp          <- DELETED BY DLS AFTER A WHILE; Intermediate files
     |_ xml          <- ARCHIVED BY DLS; Not used
-    |_ raw          <- ARCHIVED BY DLS; Raw data stored here
-    |  |_ metadata  <- Create this and save raw XML metadata file here
+    |_ raw          <- ARCHIVED BY DLS; Raw data stored here; can have multiple raws
+    |  |_ lifs
+    |  |_ metadata  <- Created by us; Save raw XML metadata file here
     |_ processed    <- ARCHIVED BY DLS; Created by us
     |  |_ raw_n     <- Following the structure of the raw folders
-    |     |_ lif_file       <- Following LIF file name
+    |     |_ lif_file_names <- Folders for data from the same LIF file
     |        |_ sub_image   <- Folders for individual sub-images
-    |           |_ tiffs    <- Save by channel
-    |           |_ metadata
-    |              |_ xml_files     <- Individual XML files
+    |           |_ tiffs    <- Save channels as individual image stacks
+    |           |_ metadata <- Individual XML files saved here (not yet implemented)
     """
-    # Set up directories
+
+    # Set up parent directories
     raw_dir = file.parent  # Raw data location
     root_dir = raw_dir.parent  # Session ID folder
 
-    # Create new directories
-    process_dir = root_dir.joinpath("processed").joinpath(
-        file.stem
-    )  # Store processing here
-    raw_xml_dir = raw_dir.joinpath("metadata")  # Raw metadata
+    # Path to new directories
+    # Save processed data here
+    process_dir = root_dir.joinpath("processed").joinpath(file.stem)
+    # Save raw XML metadata here
+    raw_xml_dir = raw_dir.joinpath("metadata")
 
     # Create new folders if not already present
     for folder in [process_dir, raw_xml_dir]:
         if not folder.exists():
             os.makedirs(str(folder))
             print(f"Created {folder}")
+            pass
         else:
             print(f"{folder} already exists")
             pass
 
     # Load LIF file as a LifFile class
     print(f"Loading {file}")
-    lf = LifFile(str(file))  # Stack of scenes
+    lif_file = LifFile(str(file))  # Stack of scenes
     print("Done")
-    scene_list = list(lf.get_iter_image())  # List of scene names
+    scene_list = list(lif_file.get_iter_image())  # List of scene names
 
     # Save original metadata as XML tree
     print("Extracting image metadata")
     xml_root = get_xml_metadata(
-        file=lf,
+        file=lif_file,
         save_xml=raw_xml_dir.joinpath(file.stem + ".xml"),
     )
     print("Done")
 
-    # Recursively generate element list of metadata
+    # Recursively generate list of metadata-containing elements
     elem_list = get_image_elements(xml_root)
 
     # Check that elements match number of images
     if not len(elem_list) == len(scene_list):
         raise Exception(
             "Error matching metadata list to list of sub-images. \n"
+            # Show what went wrong
             f"Metadata entries: {len(elem_list)} \n"
             f"Sub-images: {len(scene_list)}"
         )
@@ -249,31 +282,29 @@ def convert_lif_to_tiff(file: Path):
     # Iterate through scenes
     print("Examining sub-images")
     for i in range(len(scene_list)):
-        # Load image
-        img = lf.get_image(i)  # Set sub-image
 
-        # Load relevant metadata (name, dimensions, channels, timestamps)
+        # Load image
+        img = lif_file.get_image(i)  # Set sub-image
+
+        # Get name of sub-image
         elem = elem_list[i]  # Select corresponding element
         img_name = elem.attrib["Name"]  # Get sub-image name
         print(f"Examining {img_name}")
 
-        # Split by channel
+        # Load relevant metadata (channels, dimensions, timestamps etc.)
         channels = elem.findall(
             "Data/Image/ImageDescription/Channels/ChannelDescription"
         )
         # Might be useful in the future
         # timestamps = elem.find("Data/Image/TimeStampList")
-
-        # Might be useful in the future
-        # Common to all images
         # dimensions = elem.findall(
         #     "Data/Image/ImageDescription/Dimensions/DimensionDescription"
         # )
 
         # Create save dirs for TIFF files and their metadata
-        save_dir = process_dir.joinpath(img_name)
-        xml_dir = save_dir.joinpath("metadata")
-        for folder in [save_dir, xml_dir]:
+        img_dir = process_dir.joinpath(img_name)
+        img_xml_dir = img_dir.joinpath("metadata")
+        for folder in [img_dir, img_xml_dir]:
             if not folder.exists():
                 os.makedirs(str(folder))
                 print(f"Created {folder}")
@@ -329,30 +360,31 @@ def convert_lif_to_tiff(file: Path):
             print("Done")
 
             # Get x, y, and z scales
-            # Get resolution (pixels per distance)
-            x_res = img.scale[0]  # Pixels per um
-            y_res = img.scale[1]  # Pixels per um
+            # Get resolution (pixels per um)
+            x_res = img.scale[0]
+            y_res = img.scale[1]
 
-            # Might be used in future versions of formula
-            # x_scale = 1 / x_res  # um per pixel
-            # y_scale = 1 / y_res  # um per pixel
+            # Might be used in future versions
+            # Get pixel size (um per pixel)
+            # x_scale = 1 / x_res
+            # y_scale = 1 / y_res
 
             # Check that depth axis exists
             if not img.scale[2]:
                 z_res: float = 0
                 z_scale: float = 0  # Avoid divide by zero errors
             else:
-                z_res = img.scale[2]
-                z_scale = 1 / z_res
+                z_res = img.scale[2]  # Pixels per um
+                z_scale = 1 / z_res  # um per pixel
 
             # Generate slice labels
             image_labels = [f"{f}" for f in range(len(list(img.get_iter_z())))]
 
             # Save as a greyscale TIFF
-            tiff_name = save_dir.joinpath(color + ".tiff")
-            print(f"Saving {color.lower()} image as {tiff_name}")
+            save_name = img_dir.joinpath(color + ".tiff")
+            print(f"Saving {color.lower()} image as {save_name}")
             imwrite(
-                tiff_name,
+                save_name,
                 arr,
                 imagej=True,  # ImageJ comppatible
                 photometric="minisblack",  # Grayscale image
