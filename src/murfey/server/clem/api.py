f2f6f6b6f545b37e9da3ca8e7d2645ab5b367f7c
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2024-05-24 10:09:31 +0100

    Send LIF to TIFF conversion to a cluster (#287)
    Adds an endpoint plugin for workflows with the intention of extending in the future. Requires LIF to TIFF conversion to be packaged into a command line application

--- src/murfey/server/clem/api.py
+++ src/murfey/server/clem/api.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+import importlib.metadata
+
 from fastapi import APIRouter
 
+from murfey.server import _transport_object
 from murfey.util.lif import convert_lif_to_tiff
 from murfey.util.models import LifFileInfo
 
@@ -15,7 +18,15 @@ def lif_to_tiff(
     session_id: int,  # Used by the decorator
     lif_info: LifFileInfo,
 ):
-    convert_lif_to_tiff(
-        file=lif_info.name,
-        root_folder="images",
+    murfey_workflows = importlib.metadata.entry_points().select(
+        group="murfey.workflows", name="lif_to_tiff"
     )
+    if murfey_workflows:
+        murfey_workflows[0].load()(
+            file=lif_info.name, root_folder="images", messenger=_transport_object
+        )
+    else:
+        convert_lif_to_tiff(
+            file=lif_info.name,
+            root_folder="images",
+        )
