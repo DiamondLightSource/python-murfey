593bed0bac66fc1e3258cba70a7a2e5251515fbc
stephen-riggs <stephen.riggs@diamond.ac.uk>
2024-05-28 09:36:46 +0100

    Remove extra parts of urls for sending to the failure dlq

--- src/murfey/util/__init__.py
+++ src/murfey/util/__init__.py
@@ -11,7 +11,7 @@ from pathlib import Path
 from queue import Queue
 from threading import Thread
 from typing import Awaitable, Callable, Dict, List, Optional, Union
-from urllib.parse import ParseResult, urljoin
+from urllib.parse import ParseResult, urlparse, urlunparse
 from uuid import uuid4
 
 import requests
@@ -49,7 +49,8 @@ def capture_post(url: str, json: dict | list = {}) -> requests.Response | None:
             f"Response to post to {url} with data {json} had status code "
             f"{response.status_code}. The reason given was {response.reason}"
         )
-        failure_url = urljoin(url, "failed_client_post")
+        split_url = urlparse(url)
+        failure_url = urlunparse(split_url._replace(path="/failed_client_post"))
         try:
             resend_response = requests.post(
                 failure_url, json={"url": url, "data": json}
