f41739c9896455b5880afd146f5b581e96732d28
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2024-09-04 11:06:37 +0100

    Loosen CORS policy

--- src/murfey/hub/main.py
+++ src/murfey/hub/main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="Murfey Hub server", debug=True)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
