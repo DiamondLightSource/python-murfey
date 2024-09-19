961f7a1c799448ee0240bdf564ae57136b7a73bf
yxd92326 <stephen.riggs@diamond.ac.uk>
2024-09-19 14:46:16 +0100

    Convert bfactor fit to linear and turn it on

--- src/murfey/server/__init__.py
+++ src/murfey/server/__init__.py
@@ -2279,12 +2279,12 @@ def _save_bfactor(message: dict, _db=murfey_db, demo: bool = False):
     if all(resolutions):
         # Calculate b-factor and add to ispyb class table
         bfactor_fitting = np.polyfit(
-            np.log(particle_counts), 1 / np.array(resolutions) ** 2, 2
+            np.log(particle_counts), 1 / np.array(resolutions) ** 2, 1
         )
         refined_class_uuid = message["refined_class_uuid"]
 
         # Request an ispyb insert of the b-factor fitting parameters
-        if False and _transport_object:
+        if _transport_object:
             _transport_object.send(
                 "ispyb_connector",
                 {
@@ -2296,9 +2296,8 @@ def _save_bfactor(message: dict, _db=murfey_db, demo: bool = False):
                         "buffer_command": {
                             "ispyb_command": "insert_particle_classification"
                         },
-                        "bfactor_fit_intercept": str(bfactor_fitting[2]),
-                        "bfactor_fit_linear": str(bfactor_fitting[1]),
-                        "bfactor_fit_quadratic": str(bfactor_fitting[0]),
+                        "bfactor_fit_intercept": str(bfactor_fitting[1]),
+                        "bfactor_fit_linear": str(bfactor_fitting[0]),
                     },
                     "content": {"dummy": "dummy"},
                 },
