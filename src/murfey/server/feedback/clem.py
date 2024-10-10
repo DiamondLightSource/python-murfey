276ee54bbec9360c534e95811657953849958f06
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2024-10-10 18:53:49 +0100

    Added folder to store module feedback callback workflows in

---
+++ src/murfey/server/feedback/clem.py
@@ -0,0 +1,6 @@
+"""
+Functions to process the requests received by Murfey related to the CLEM workflow.
+
+The CLEM-related file registration API endpoints can eventually be moved here, since
+the file registration processes all take place on the server side only.
+"""
