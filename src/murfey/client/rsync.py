14d945215299c66294d884fd63ff19bbde089db4
Eu-Pin Tien <eu-pin.tien@diamond.ac.uk>
2024-10-29 14:30:16 +0000

    Replaced 'procrunner' with 'subprocess' Co-authored by: stephen-riggs <stephen.riggs@diamond.ac.uk>

--- src/murfey/client/rsync.py
+++ src/murfey/client/rsync.py
@@ -18,8 +18,6 @@ from pathlib import Path
 from typing import Callable, List, NamedTuple
 from urllib.parse import ParseResult
 
-import procrunner
-
 from murfey.client.tui.status_bar import StatusBar
 from murfey.util import Observer
 
@@ -433,28 +431,29 @@ class RSyncer(Observer):
         result: subprocess.CompletedProcess | None = None
         success = True
         if rsync_stdin:
-            result = procrunner.run(
+            result = subprocess.run(
                 rsync_cmd,
-                callback_stdout=parse_stdout,
-                callback_stderr=parse_stderr,
-                working_directory=str(self._basepath),
-                stdin=rsync_stdin,
-                print_stdout=False,
-                print_stderr=False,
+                cwd=str(self._basepath),
+                capture_output=True,
+                input=rsync_stdin,
             )
-            success = result.returncode == 0 if result else False
+            for stdout_line in result.stdout.decode("utf8", "replace").split("\n"):
+                parse_stdout(stdout_line)
+            for stderr_line in result.stderr.decode("utf8", "replace").split("\n"):
+                parse_stderr(stderr_line)
+            success = result.returncode == 0
 
         if rsync_stdin_remove:
             rsync_cmd.insert(-2, "--remove-source-files")
-            result = procrunner.run(
+            result = subprocess.run(
                 rsync_cmd,
-                callback_stdout=parse_stdout,
-                callback_stderr=parse_stderr,
-                working_directory=str(self._basepath),
-                stdin=rsync_stdin_remove,
-                print_stdout=False,
-                print_stderr=False,
+                cwd=str(self._basepath),
+                input=rsync_stdin_remove,
             )
+            for stdout_line in result.stdout.decode("utf8", "replace").split("\n"):
+                parse_stdout(stdout_line)
+            for stderr_line in result.stderr.decode("utf8", "replace").split("\n"):
+                parse_stderr(stderr_line)
 
             if success:
                 success = result.returncode == 0 if result else False
