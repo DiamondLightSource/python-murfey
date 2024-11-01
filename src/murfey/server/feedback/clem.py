c4bdfc7de23cccd527447faf7666b8a264b490bd
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2024-11-01 16:31:47 +0000

    Missed deleting the old file

--- src/murfey/server/feedback/clem.py
+++
@@ -1,6 +0,0 @@
-"""
-Functions to process the requests received by Murfey related to the CLEM workflow.
-
-The CLEM-related file registration API endpoints can eventually be moved here, since
-the file registration processes all take place on the server side only.
-"""
