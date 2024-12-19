d43ab647cbca3f67bcc7ec63ccf7956cf743d7dd
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2024-12-19 18:44:51 +0000

    Fixed improperly cased 'MURFEY_GLOBAL_CONFIGURATION'

--- tests/cli/test_generate_password.py
+++ tests/cli/test_generate_password.py
@@ -13,7 +13,7 @@ def test_generate_password(capsys, tmp_path):
     global_config.crypto_key = crypto_key.decode("ascii")
     with open(tmp_path / "config.yaml", "w") as cfg:
         yaml.dump(global_config.dict(), cfg)
-    os.environ["MURFEY_global_configURATION"] = str(tmp_path / "config.yaml")
+    os.environ["MURFEY_GLOBAL_CONFIGURATION"] = str(tmp_path / "config.yaml")
     run()
     captured = capsys.readouterr()
     f = Fernet(crypto_key)
