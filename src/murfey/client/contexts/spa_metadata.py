8f05d01b954c354762309f860e69f6d7b63086b1
Dan Hatton <daniel.hatton@diamond.ac.uk>
2025-01-17 15:23:02 +0000

    On reading EpuSession.dm post all the grid squares found

--- src/murfey/client/contexts/spa_metadata.py
+++ src/murfey/client/contexts/spa_metadata.py
@@ -117,37 +117,22 @@ class SPAMetadataContext(Context):
                     "atlas_pixel_size": atlas_pixel_size,
                 }
                 capture_post(url, json=dcg_data)
-                registered_grid_squares = (
-                    requests.get(
-                        f"{str(environment.url.geturl())}/sessions/{environment.murfey_session}/grid_squares"
-                    )
-                    .json()
-                    .get(str(source), [])
+                gs_pix_positions = _get_grid_square_atlas_positions(
+                    _atlas_destination(environment, source, transferred_file)
+                    / environment.samples[source].atlas
                 )
-                if registered_grid_squares:
-                    gs_pix_positions = _get_grid_square_atlas_positions(
-                        _atlas_destination(environment, source, transferred_file)
-                        / environment.samples[source].atlas
-                    )
-                    for gs in registered_grid_squares:
-                        pos_data = gs_pix_positions.get(str(gs["name"]))
-                        if pos_data:
-                            capture_post(
-                                f"{str(environment.url.geturl())}/sessions/{environment.murfey_session}/grid_square/{gs['name']}",
-                                json={
-                                    "tag": gs["tag"],
-                                    "readout_area_x": gs["readout_area_x"],
-                                    "readout_area_y": gs["readout_area_y"],
-                                    "thumbnail_size_x": gs["thumbnail_size_x"],
-                                    "thumbnail_size_y": gs["thumbnail_size_y"],
-                                    "pixel_size": gs["pixel_size"],
-                                    "image": gs["image"],
-                                    "x_location": pos_data[0],
-                                    "y_location": pos_data[1],
-                                    "x_stage_position": pos_data[2],
-                                    "y_stage_position": pos_data[3],
-                                    "width": pos_data[4],
-                                    "height": pos_data[5],
-                                    "angle": pos_data[6],
-                                },
-                            )
+                for gs, pos_data in gs_pix_positions.items():
+                    if pos_data:
+                        capture_post(
+                            f"{str(environment.url.geturl())}/sessions/{environment.murfey_session}/grid_square/{gs}",
+                            json={
+                                "tag": str(source),
+                                "x_location": pos_data[0],
+                                "y_location": pos_data[1],
+                                "x_stage_position": pos_data[2],
+                                "y_stage_position": pos_data[3],
+                                "width": pos_data[4],
+                                "height": pos_data[5],
+                                "angle": pos_data[6],
+                            },
+                        )
