005d81c09a1cdfe42634d5c02dcebe1c964da56c
yxd92326 <stephen.riggs@diamond.ac.uk>
2025-01-24 10:25:38 +0000

    add session to test db

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
 import pytest
 
-from murfey.util.db import clear, setup
-from tests import url
+from murfey.util.db import Session, clear, setup
+from tests import engine, url
 
 
 @pytest.fixture
 def start_postgres():
     clear(url)
     setup(url)
+
+    murfey_session = Session(id=2, name="cm12345-6")
+    with Session(engine) as murfey_db:
+        murfey_db.add(murfey_session)
+        murfey_db.commit()
