b246c1a47ab3bc1c59e781eb9730afcfe1010789
yxd92326 <stephen.riggs@diamond.ac.uk>
2025-01-24 09:57:29 +0000

    Might be running on localhost

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,7 +3,7 @@ import os
 from sqlmodel import create_engine
 
 url = (
-    f"postgresql://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}"
+    f"postgresql+psycopg2://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}"
     f"@{os.environ['POSTGRES_HOST']}:{os.environ['POSTGRES_PORT']}/{os.environ['POSTGRES_DB']}"
 )
 
