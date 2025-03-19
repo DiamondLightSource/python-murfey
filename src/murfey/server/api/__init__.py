458315c73351c2fefb2fa8a7661a398a1d60f730
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2025-03-19 12:06:18 +0000

    Add mag table endpoints

--- src/murfey/server/api/__init__.py
+++ src/murfey/server/api/__init__.py
@@ -59,6 +59,7 @@ from murfey.util.db import (
     DataCollectionGroup,
     FoilHole,
     GridSquare,
+    MagnificationLookup,
     Movie,
     PreprocessStash,
     ProcessingJob,
@@ -164,6 +165,27 @@ def machine_info_by_name(instrument_name: str) -> Optional[MachineConfig]:
             instrument_name
         ]
     return None
+
+
+@router.get("/mag_table/")
+def get_mag_table(db=murfey_db) -> List[MagnificationLookup]:
+    return db.exec(select(MagnificationLookup)).all()
+
+
+@router.post("/mag_table/")
+def add_to_mag_table(rows: List[MagnificationLookup], db=murfey_db):
+    for r in rows:
+        db.add(r)
+    db.commit()
+
+
+@router.delete("/mag_table/{mag}")
+def remove_mag_table_row(mag: int, db=murfey_db):
+    row = db.exec(
+        select(MagnificationLookup).where(MagnificationLookup.magnification == mag)
+    ).one()
+    db.delete(row)
+    db.commit()
 
 
 @router.get("/instruments/{instrument_name}/instrument_name")
