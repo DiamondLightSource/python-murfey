64e44674b1fce6387b8ffc1a09ce16b63cf87000
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2025-03-19 10:33:11 +0000

    Setup logging to graylog for instrument server via main server

--- src/murfey/instrument_server/__init__.py
+++ src/murfey/instrument_server/__init__.py
@@ -5,6 +5,8 @@ import uvicorn
 from rich.logging import RichHandler
 
 import murfey
+from murfey.client import read_config
+from murfey.client.customlogging import CustomHandler
 from murfey.util import LogFilter
 
 logger = logging.getLogger("murfey.instrument_server")
@@ -32,6 +34,13 @@ def run():
     logging.getLogger("murfey").addHandler(rich_handler)
     logging.getLogger("fastapi").addHandler(rich_handler)
     logging.getLogger("uvicorn").addHandler(rich_handler)
+
+    ws = murfey.client.websocket.WSApp(
+        server=read_config()["Murfey"].get("server"),
+    )
+
+    handler = CustomHandler(ws.send)
+    logging.getLogger().addHandler(handler)
 
     logger.info(
         f"Starting Murfey server version {murfey.__version__}, listening on {args.host}:{args.port}"
