ac91f96f7ab575d31aeba9c30a9805bb00a337c4
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-04-10 17:07:02 +0100

    Added unit test for 'read_config()' function

--- tests/util/test_client.py
+++ tests/util/test_client.py
@@ -1,6 +1,60 @@
 import json
+import os
+from pathlib import Path
+from unittest.mock import patch
 
-from murfey.util.client import set_default_acquisition_output
+from pytest import mark
+
+from murfey.util.client import read_config, set_default_acquisition_output
+
+test_read_config_params_matrix = (
+    # Environment variable to set | Append to tmp_path
+    (
+        "MURFEY_CLIENT_CONFIGURATION",
+        "config/murfey-client-config.cfg",
+    ),
+    (
+        "MURFEY_CLIENT_CONFIG_HOME",
+        "config",
+    ),
+    (
+        "",
+        "",
+    ),  # Test default home directory
+)
+
+
+@mark.parametrize("test_params", test_read_config_params_matrix)
+def test_read_config(
+    test_params: tuple[str, str],
+    tmp_path,
+    mock_client_configuration,
+):
+    # Unpack test params
+    env_var, partial_path = test_params
+
+    # Construct the environment variable and the expected config file path
+    env_var_dict: dict[str, str] = {}
+    if env_var:
+        full_path = tmp_path / partial_path
+        env_var_dict[env_var] = str(full_path)
+        file_path = full_path if full_path.suffix() else full_path / ".murfey"
+    else:
+        file_path = Path().home() / ".murfey"
+
+    # Make directories all the way to the requested place
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+
+    # Write the client config fixture to the specified file
+    with open(file_path, "w") as file:
+        mock_client_configuration.write(file)
+
+    # Patch the OS environment variable and run the function
+    with patch.dict(os.environ, env_var_dict, clear=False):
+        config = read_config()
+
+    # Compare returned config with mock one
+    assert dict(config["Murfey"]) == dict(mock_client_configuration["Murfey"])
 
 
 def test_set_default_acquisition_output_normal_operation(tmp_path):
