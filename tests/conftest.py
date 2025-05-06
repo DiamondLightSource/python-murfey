97dc116821bd2866b8cdcf656522df3b76f97a5e
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-05-06 15:02:19 +0100

    Added fixture for ISPyB credentials file; modified security configuration file fixture to return file path as part of the fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,22 @@
 import json
 from configparser import ConfigParser
+from pathlib import Path
 
 import pytest
 from sqlmodel import Session
 
 from murfey.util.db import Session as MurfeySession
 from murfey.util.db import clear, setup
-from tests import engine, url
-
-mock_security_config_name = "security_config.yaml"
+from tests import murfey_db_engine, murfey_db_url
 
 
 @pytest.fixture
 def start_postgres():
-    clear(url)
-    setup(url)
+    clear(murfey_db_url)
+    setup(murfey_db_url)
 
     murfey_session = MurfeySession(id=2, name="cm12345-6")
-    with Session(engine) as murfey_db:
+    with Session(murfey_db_engine) as murfey_db:
         murfey_db.add(murfey_session)
         murfey_db.commit()
 
@@ -37,15 +36,37 @@ def mock_client_configuration() -> ConfigParser:
 
 
 @pytest.fixture()
-def mock_security_configuration(tmp_path):
-    config_file = tmp_path / mock_security_config_name
+def mock_ispyb_credentials(tmp_path: Path):
+    creds_file = tmp_path / "ispyb_creds.cfg"
+    ispyb_config = ConfigParser()
+    # Use values from the GitHub workflow ISPyB config file
+    ispyb_config["ispyb_sqlalchemy"] = {
+        "username": "ispyb_api_sqlalchemy",
+        "password": "password_5678",
+        "host": "localhost",
+        "port": "3306",
+        "database": "ispybtest",
+    }
+    with open(creds_file, "w") as file:
+        ispyb_config.write(file)
+    return creds_file
+
+
+@pytest.fixture()
+def mock_security_configuration(
+    tmp_path: Path,
+    mock_ispyb_credentials: Path,
+):
+    config_file = tmp_path / "security_config.yaml"
     security_config = {
-        "auth_key": "auth_key",
-        "auth_algorithm": "auth_algorithm",
-        "feedback_queue": "murfey_feedback",
-        "rabbitmq_credentials": "/path/to/rabbitmq.yaml",
         "murfey_db_credentials": "/path/to/murfey_db_credentials",
         "crypto_key": "crypto_key",
+        "auth_key": "auth_key",
+        "auth_algorithm": "auth_algorithm",
+        "rabbitmq_credentials": "/path/to/rabbitmq.yaml",
+        "feedback_queue": "murfey_feedback",
+        "ispyb_credentials": str(mock_ispyb_credentials),
     }
     with open(config_file, "w") as f:
         json.dump(security_config, f)
+    return config_file
