03897de669c766725d60bd1376740ea38ccba3dd
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-05-06 15:00:48 +0100

    Renamed URL and Engine for Murfey DB to make it more self-evident

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,9 +2,9 @@ import os
 
 from sqlmodel import create_engine
 
-url = (
+murfey_db_url = (
     f"postgresql+psycopg2://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}"
     f"@{os.environ['POSTGRES_HOST']}:{os.environ['POSTGRES_PORT']}/{os.environ['POSTGRES_DB']}"
 )
 
-engine = create_engine(url)
+murfey_db_engine = create_engine(murfey_db_url)
