cabec6f63f3f6ff4456fabc98426c0f1ed68bd6a
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-05-07 13:35:41 +0100

    Added unit test for 'get_proposal_id()' function

--- tests/server/test_ispyb.py
+++ tests/server/test_ispyb.py
@@ -2,7 +2,7 @@ from ispyb.sqlalchemy import BLSession, Proposal
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from murfey.server.ispyb import get_session_id
+from murfey.server.ispyb import get_proposal_id, get_session_id
 from tests.conftest import ExampleVisit
 
 
@@ -31,5 +31,27 @@ def test_get_session_id(
         proposal_number=str(ExampleVisit.proposal_number),
         visit_number=str(ExampleVisit.visit_number),
         db=ispyb_db,
+    )
+    assert query == result
+
+
+def test_get_proposal_id(
+    ispyb_db: Session,
+):
+    # Manually query the Proposal ID
+    query = (
+        ispyb_db.execute(
+            select(Proposal)
+            .where(Proposal.proposalCode == ExampleVisit.proposal_code)
+            .where(Proposal.proposalNumber == ExampleVisit.proposal_number)
+        )
+        .scalar_one()
+        .proposalId
+    )
+
+    # Test function
+    result = get_proposal_id(
+        proposal_code=ExampleVisit.proposal_code,
+        proposal_number=ExampleVisit.proposal_number,
     )
     assert query == result
