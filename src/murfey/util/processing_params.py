44fe0215e5a3f1cda71aa2f0e7768420afcdc715
yxd92326 <stephen.riggs@diamond.ac.uk>
2025-05-29 15:18:28 +0100

    Move cryolo model function to util

--- src/murfey/util/processing_params.py
+++ src/murfey/util/processing_params.py
@@ -1,6 +1,28 @@
+from datetime import datetime
+from functools import lru_cache
+from pathlib import Path
 from typing import Literal, Optional
 
 from pydantic import BaseModel
+
+from murfey.util.config import get_machine_config
+
+
+@lru_cache(maxsize=5)
+def _cryolo_model_path(visit: str, instrument_name: str) -> Path:
+    machine_config = get_machine_config(instrument_name=instrument_name)[
+        instrument_name
+    ]
+    if machine_config.model_search_directory:
+        visit_directory = (
+            machine_config.rsync_basepath / str(datetime.now().year) / visit
+        )
+        possible_models = list(
+            (visit_directory / machine_config.model_search_directory).glob("*.h5")
+        )
+        if possible_models:
+            return sorted(possible_models, key=lambda x: x.stat().st_ctime)[-1]
+    return machine_config.default_model
 
 
 class CLEMAlignAndMergeParameters(BaseModel):
