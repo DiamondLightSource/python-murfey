de4a73a9359e6990edbecd7436a988dffd96fd15
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-06-05 14:33:38 +0100

    Test funciton directly (ノಠ益ಠ)ノ彡┻━┻

--- tests/server/api/test_movies.py
+++ tests/server/api/test_movies.py
@@ -1,12 +1,6 @@
-from unittest.mock import ANY, patch
-
-from fastapi.testclient import TestClient
 from sqlmodel import Session
 
-from murfey.server.api.auth import validate_instrument_token
-from murfey.server.main import app
-from murfey.util.api import url_path_for
-from murfey.util.config import security_from_file
+from murfey.server.api.session_control import count_number_of_movies
 from murfey.util.db import (
     AutoProcProgram,
     DataCollection,
@@ -15,125 +9,74 @@ from murfey.util.db import (
     MurfeyLedger,
     ProcessingJob,
 )
-from tests.conftest import ExampleVisit, get_or_create_db_entry, murfey_db_url
-
-# @pytest.fixture(scope="module")
-# def test_user():
-#     return {"username": "testuser", "password": "testpass"}
-
-
-# def movies_return():
-#     return [("Supervisor_1", 2)]
-
-
-# expression = Mock()
-# expression.all = movies_return
-
-# mock_session = create_autospec(Session, instance=True)
-# mock_session.exec.return_value = expression
-
-
-# def override_murfey_db():
-#     try:
-#         db = mock_session
-#         yield db
-#     finally:
-#         db.close()
-
-
-# app.dependency_overrides[murfey_db] = override_murfey_db
+from tests.conftest import ExampleVisit, get_or_create_db_entry
 
 
 def test_movie_count(
-    mock_security_configuration,  # From conftest.py
     murfey_db_session: Session,  # From conftest.py
 ):
 
-    # Set up mock security configs for
-    with (
-        patch(
-            "murfey.server.murfey_db.get_security_config"
-        ) as mock_get_security_config,
-        patch("murfey.server.murfey_db.url") as mock_get_url,
-    ):
+    # Insert table dependencies
+    dcg_entry: DataCollectionGroup = get_or_create_db_entry(
+        murfey_db_session,
+        DataCollectionGroup,
+        lookup_kwargs={
+            "id": 0,
+            "session_id": ExampleVisit.murfey_session_id,
+            "tag": "test_dcg",
+        },
+    )
+    dc_entry: DataCollection = get_or_create_db_entry(
+        murfey_db_session,
+        DataCollection,
+        lookup_kwargs={
+            "id": 0,
+            "tag": "test_dc",
+            "dcg_id": dcg_entry.id,
+        },
+    )
+    processing_job_entry: ProcessingJob = get_or_create_db_entry(
+        murfey_db_session,
+        ProcessingJob,
+        lookup_kwargs={
+            "id": 0,
+            "recipe": "test_recipe",
+            "dc_id": dc_entry.id,
+        },
+    )
+    autoproc_entry: AutoProcProgram = get_or_create_db_entry(
+        murfey_db_session,
+        AutoProcProgram,
+        lookup_kwargs={
+            "id": 0,
+            "pj_id": processing_job_entry.id,
+        },
+    )
 
-        # Defer import of 'murfey_db' until after mocks are configured
-        mock_get_url.return_value = murfey_db_url
-        mock_get_security_config.return_value = security_from_file(
-            mock_security_configuration
+    # Insert test movies and one-to-one dependencies into Murfey DB
+    tag = "test_movie"
+    num_movies = 5
+    murfey_db_session
+    for i in range(num_movies):
+        murfey_ledger_entry: MurfeyLedger = get_or_create_db_entry(
+            murfey_db_session,
+            MurfeyLedger,
+            lookup_kwargs={
+                "id": i,
+                "app_id": autoproc_entry.id,
+            },
         )
-        from murfey.server.murfey_db import murfey_db
+        _: Movie = get_or_create_db_entry(
+            murfey_db_session,
+            Movie,
+            lookup_kwargs={
+                "murfey_id": murfey_ledger_entry.id,
+                "path": "/some/path",
+                "image_number": i,
+                "tag": tag,
+            },
+        )
 
-        # Replace the murfey_db instance in endpoint with properly initialised pytest one
-        app.dependency_overrides[murfey_db] = murfey_db_session
-        # Disable instrument token validation
-        app.dependency_overrides[validate_instrument_token] = lambda: None
-
-        with TestClient(app) as client:
-
-            # Insert table dependencies
-            dcg_entry: DataCollectionGroup = get_or_create_db_entry(
-                murfey_db_session,
-                DataCollectionGroup,
-                lookup_kwargs={
-                    "id": 0,
-                    "session_id": ExampleVisit.murfey_session_id,
-                    "tag": "test_dcg",
-                },
-            )
-            dc_entry: DataCollection = get_or_create_db_entry(
-                murfey_db_session,
-                DataCollection,
-                lookup_kwargs={
-                    "id": 0,
-                    "tag": "test_dc",
-                    "dcg_id": dcg_entry.id,
-                },
-            )
-            processing_job_entry: ProcessingJob = get_or_create_db_entry(
-                murfey_db_session,
-                ProcessingJob,
-                lookup_kwargs={
-                    "id": 0,
-                    "recipe": "test_recipe",
-                    "dc_id": dc_entry.id,
-                },
-            )
-            autoproc_entry: AutoProcProgram = get_or_create_db_entry(
-                murfey_db_session,
-                AutoProcProgram,
-                lookup_kwargs={
-                    "id": 0,
-                    "pj_id": processing_job_entry.id,
-                },
-            )
-
-            # Insert test movies and one-to-one dependencies into Murfey DB
-            tag = "test_movie"
-            num_movies = 5
-            murfey_db_session
-            for i in range(num_movies):
-                murfey_ledger_entry: MurfeyLedger = get_or_create_db_entry(
-                    murfey_db_session,
-                    MurfeyLedger,
-                    lookup_kwargs={
-                        "id": i,
-                        "app_id": autoproc_entry.id,
-                    },
-                )
-                _: Movie = get_or_create_db_entry(
-                    murfey_db_session,
-                    Movie,
-                    lookup_kwargs={
-                        "murfey_id": murfey_ledger_entry.id,
-                        "path": "/some/path",
-                        "image_number": i,
-                        "tag": tag,
-                    },
-                )
-
-            response = client.get(
-                f"{url_path_for('session_control.router', 'count_number_of_movies')}",
-                headers={"Authorization": f"Bearer {ANY}"},
-            )
-            assert response.json() == {tag: num_movies}
+    # Run function and evaluate result
+    result = count_number_of_movies(murfey_db_session)
+    assert result == {tag: num_movies}
