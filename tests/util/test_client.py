cda4d3590fb54f08a6e751fc4a19f6d80e9c034f
Eu-Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-07-08 17:02:47 +0100

    Migrated tests to reflect refactoring of 'murfey.client' submodules

--- tests/util/test_client.py
+++ tests/util/test_client.py
@@ -1,14 +1,11 @@
 import json
 import os
 from pathlib import Path
-from unittest.mock import Mock, patch
-from urllib.parse import urlparse
+from unittest import mock
 
-from pytest import mark
+import pytest
 
-from murfey.client import _get_visit_list
 from murfey.util.client import read_config, set_default_acquisition_output
-from murfey.util.models import Visit
 
 test_read_config_params_matrix = (
     # Environment variable to set | Append to tmp_path
@@ -27,7 +24,7 @@ test_read_config_params_matrix = (
 )
 
 
-@mark.parametrize("test_params", test_read_config_params_matrix)
+@pytest.mark.parametrize("test_params", test_read_config_params_matrix)
 def test_read_config(
     test_params: tuple[str, str],
     tmp_path,
@@ -53,70 +50,11 @@ def test_read_config(
         mock_client_configuration.write(file)
 
     # Patch the OS environment variable and run the function
-    with patch.dict(os.environ, env_var_dict, clear=False):
+    with mock.patch.dict(os.environ, env_var_dict, clear=False):
         config = read_config()
 
     # Compare returned config with mock one
     assert dict(config["Murfey"]) == dict(mock_client_configuration["Murfey"])
-
-
-test_get_visit_list_params_matrix = (
-    ("http://0.0.0.0:8000",),
-    ("http://0.0.0.0:8000/api",),
-    ("http://murfey_server",),
-    ("http://murfey_server/api",),
-    ("http://murfey_server.com",),
-)
-
-
-@mark.parametrize("test_params", test_get_visit_list_params_matrix)
-@patch("murfey.client.requests")
-def test_get_visit_list(
-    mock_request,
-    test_params: tuple[str],
-    mock_client_configuration,
-):
-    # Unpack test params and set up other params
-    (server_url,) = test_params
-    instrument_name = mock_client_configuration["Murfey"]["instrument_name"]
-
-    # Construct the expected request response
-    example_visits = [
-        {
-            "start": "1999-09-09T09:00:00",
-            "end": "1999-09-11T09:00:00",
-            "session_id": 123456789,
-            "name": "cm12345-0",
-            "beamline": "murfey",
-            "proposal_title": "Commissioning Session 1",
-        },
-        {
-            "start": "1999-09-09T09:00:00",
-            "end": "1999-09-11T09:00:00",
-            "session_id": 246913578,
-            "name": "cm23456-1",
-            "beamline": "murfey",
-            "proposal_title": "Cryo-cycle 1999",
-        },
-    ]
-    mock_response = Mock()
-    mock_response.status_code = 200
-    mock_response.json.return_value = example_visits
-    mock_request.get.return_value = mock_response
-
-    # read_config() has to be patched using fixture, so has to be done in function
-    with patch("murfey.util.client.read_config", mock_client_configuration):
-        visits = _get_visit_list(urlparse(server_url), instrument_name)
-
-    # Check that request was sent with the correct URL
-    expected_url = (
-        f"{server_url}/session_control/instruments/{instrument_name}/visits_raw"
-    )
-    mock_request.get.assert_called_once_with(expected_url)
-
-    # Check that expected outputs are correct (order-sensitive)
-    for v, visit in enumerate(visits):
-        assert visit.dict() == Visit.parse_obj(example_visits[v]).dict()
 
 
 def test_set_default_acquisition_output_normal_operation(tmp_path):
