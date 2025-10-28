a42f8d9a1f0e443b3c27b456e4261e1dcb809695
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-10-28 18:00:12 +0000

    Import module for test only after mocking out the functions used to generate the module-level variables

--- tests/server/test_feedback.py
+++ tests/server/test_feedback.py
@@ -1,11 +1,8 @@
-from unittest import mock
 from unittest.mock import MagicMock
 
 import pytest
 from backports.entry_points_selectable import entry_points
 from pytest_mock import MockerFixture
-
-from murfey.server.feedback import feedback_callback
 
 feedback_callback_params_matrix = (
     # Murfey workflows currently present in pyproject.toml
@@ -37,17 +34,15 @@ def test_feedback_callback(
     # Unpack test params
     (entry_point_name,) = test_params
 
-    # Mock the Murfey database creation in the main body of the module
-    mock_get_security_config = mocker.patch(
-        "murfey.server.feedback.get_security_config"
-    )
+    # Patch the functions used to generate the module-level variables
+    mock_get_security_config = mocker.patch("murfey.util.config.get_security_config")
     mock_get_security_config.return_value = MagicMock()
-    mock_url = mocker.patch("murfey.server.feedback.url")
-    mock_url.return_value = mock.sentinel
-    mock_create_engine = mocker.patch("murfey.server.feedback.create_engine")
+    mock_url = mocker.patch("murfey.server.murfey_db.url")
+    mock_url.return_value = MagicMock()
+    mock_create_engine = mocker.patch("sqlmodel.create_engine")
     mock_create_engine.return_value = MagicMock()
     mock_murfey_db = MagicMock()
-    mock_sql_session = mocker.patch("murfey.server.feedback.Session")
+    mock_sql_session = mocker.patch("sqlmodel.Session")
     mock_sql_session.return_value = mock_murfey_db
 
     # Load the entry point and patch the executable it calls
@@ -55,9 +50,11 @@ def test_feedback_callback(
     assert len(eps) == 1  # Entry point should be present and unique
     mock_function = mocker.patch(eps[0].value.replace(":", "."))
 
-    header = {"dummy": "dummy"}
-    message = {"register": entry_point_name}
+    # Initialise after mocking
+    from murfey.server.feedback import feedback_callback
 
     # Run the function and check that it calls the entry point correctly
+    header = {"dummy": "dummy"}
+    message = {"register": entry_point_name}
     feedback_callback(header, message, mock_murfey_db)
     mock_function.assert_called_once_with(message=message, murfey_db=mock_murfey_db)
