7967068aa9cf90459043b4071a624ec839588d3b
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-11-05 12:00:09 +0000

    Added placeholders for the unit tests to register CLEM preprocessing results

---
+++ tests/workflows/clem/test_register_preprocessing_results.py
@@ -0,0 +1,28 @@
+import pytest
+
+from murfey.workflows.clem.register_preprocessing_results import (
+    _register_clem_image_series,
+    _register_dcg_and_atlas,
+    _register_grid_square,
+    run,
+)
+
+
+@pytest.mark.skip
+def test_register_clem_image_series():
+    assert _register_clem_image_series
+
+
+@pytest.mark.skip
+def test_register_dcg_and_atlas():
+    assert _register_dcg_and_atlas
+
+
+@pytest.mark.skip
+def test_register_grid_square():
+    assert _register_grid_square
+
+
+@pytest.mark.skip
+def test_run():
+    assert run
