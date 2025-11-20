babfea4b0753b08cd0a456af09dd0541e102469c
yxd92326 <stephen.riggs@diamond.ac.uk>
2025-11-20 16:02:49 +0000

    Register dcg for atlases

--- src/murfey/client/contexts/atlas.py
+++ src/murfey/client/contexts/atlas.py
@@ -2,6 +2,8 @@ import logging
 from pathlib import Path
 from typing import Optional
 
+import xmltodict
+
 from murfey.client.context import Context
 from murfey.client.contexts.spa import _get_source
 from murfey.client.contexts.spa_metadata import _atlas_destination
@@ -48,4 +50,56 @@ class AtlasContext(Context):
                 )
                 logger.info(
                     f"Submitted request to create JPG image of atlas {str(transferred_atlas_name)!r}"
+                )
+        elif (
+            environment
+            and "Atlas_" in transferred_file.stem
+            and transferred_file.suffix == ".xml"
+        ):
+            source = _get_source(transferred_file, environment)
+            if source:
+                atlas_mrc = transferred_file.with_suffix(".mrc")
+                transferred_atlas_name = _atlas_destination(
+                    environment, source, atlas_mrc, self._token
+                ) / atlas_mrc.relative_to(source.parent)
+
+                with open(transferred_file, "rb") as atlas_xml:
+                    atlas_xml_data = xmltodict.parse(atlas_xml)
+                    atlas_original_pixel_size = float(
+                        atlas_xml_data["MicroscopeImage"]["SpatialScale"]["pixelSize"][
+                            "x"
+                        ]["numericValue"]
+                    )
+
+                # need to calculate the pixel size of the downscaled image
+                atlas_pixel_size = atlas_original_pixel_size * 7.8
+
+                for p in transferred_file.parts:
+                    if p.startswith("Sample"):
+                        sample = int(p.replace("Sample", ""))
+                        break
+                else:
+                    logger.warning(
+                        f"Sample could not be identified for {transferred_file}"
+                    )
+                    return
+
+                dcg_data = {
+                    "experiment_type_id": 44,  # Atlas
+                    "tag": str(transferred_file.parent),
+                    "atlas": str(transferred_atlas_name),
+                    "sample": sample,
+                    "atlas_pixel_size": atlas_pixel_size,
+                }
+                capture_post(
+                    base_url=str(environment.url.geturl()),
+                    router_name="workflow.router",
+                    function_name="register_dc_group",
+                    token=self._token,
+                    visit_name=environment.visit,
+                    session_id=environment.murfey_session,
+                    data=dcg_data,
+                )
+                logger.info(
+                    f"Registered data collection group for atlas {str(transferred_atlas_name)!r}"
                 )
