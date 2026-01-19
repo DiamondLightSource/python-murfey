e4a0d7834be23e572baaca864c2edadbc76a49e7
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2026-01-19 12:56:07 +0000

    Decorated async test stubs

--- tests/server/api/test_auth_api.py
+++ tests/server/api/test_auth_api.py
@@ -1,11 +1,16 @@
+import pytest
+
+
 def test_check_user():
     pass
 
 
+@pytest.mark.asyncio
 async def test_submit_to_auth_endpoint():
     pass
 
 
+@pytest.mark.asyncio
 async def test_validate_token():
     pass
 
@@ -14,6 +19,7 @@ def test_validate_session_against_visit():
     pass
 
 
+@pytest.mark.asyncio
 async def test_validate_instrument_token():
     pass
 
@@ -22,14 +28,17 @@ def test_get_visit_name():
     pass
 
 
+@pytest.mark.asyncio
 async def test_validate_instrument_server_session_access():
     pass
 
 
+@pytest.mark.asyncio
 async def test_validate_frontend_session_access():
     pass
 
 
+@pytest.mark.asyncio
 async def test_validate_user_instrument_access():
     pass
 
@@ -46,9 +55,11 @@ def test_create_access_token():
     pass
 
 
+@pytest.mark.asyncio
 async def test_generate_token():
     pass
 
 
+@pytest.mark.asyncio
 async def test_mint_session_token():
     pass
