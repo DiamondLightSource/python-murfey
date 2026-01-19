3b26241f60adbe6dc47ff23a13557912fc93eefe
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2026-01-19 11:03:58 +0000

    Add placeholder tests for auth functions and API endpoints

---
+++ tests/server/api/test_auth_api.py
@@ -0,0 +1,54 @@
+def test_check_user():
+    pass
+
+
+async def test_submit_to_auth_endpoint():
+    pass
+
+
+async def test_validate_token():
+    pass
+
+
+def test_validate_session_against_visit():
+    pass
+
+
+async def test_validate_instrument_token():
+    pass
+
+
+def test_get_visit_name():
+    pass
+
+
+async def test_validate_instrument_server_session_access():
+    pass
+
+
+async def test_validate_frontend_session_access():
+    pass
+
+
+async def test_validate_user_instrument_access():
+    pass
+
+
+def test_verify_password():
+    pass
+
+
+def test_validate_user():
+    pass
+
+
+def test_create_access_token():
+    pass
+
+
+async def test_generate_token():
+    pass
+
+
+async def test_mint_session_token():
+    pass
