93146b660af88506e1cbb3ed8c4e8fb71932951c
yxd92326 <stephen.riggs@diamond.ac.uk>
2026-02-11 11:49:36 +0000

    Do not update scaup for atlases if two dcgs exist

--- src/murfey/workflows/register_atlas_update.py
+++ src/murfey/workflows/register_atlas_update.py
@@ -1,9 +1,12 @@
 import logging
 from importlib.metadata import entry_points
+from pathlib import Path
 
+from sqlmodel import select
 from sqlmodel.orm.session import Session as SQLModelSession
 
 from murfey.server import _transport_object
+from murfey.util.db import DataCollectionGroup
 
 logger = logging.getLogger("murfey.workflows.register_atlas_update")
 
@@ -24,6 +27,24 @@ def run(
         message["atlas_pixel_size"],
         message["sample"],
     )
+
+    # Find out how many dcgs we have with this atlas
+    if (
+        message.get("atlas")
+        and message.get("sample")
+        and "atlas" in Path(message.get("tag", "/")).parts
+    ):
+        dcgs_atlas = murfey_db.exec(
+            select(DataCollectionGroup)
+            .where(DataCollectionGroup.session_id == message["session_id"])
+            .where(DataCollectionGroup.atlas == message["atlas"])
+            .where(DataCollectionGroup.sample == message["sample"])
+        ).all()
+        if len(dcgs_atlas) > 1:
+            # Skip hooks if this is an atlas and there is a processing dcg present
+            logger.info(f"Skipping data collection group hooks for {message['tag']}")
+            return {"success": True}
+
     if dcg_hooks := entry_points(group="murfey.hooks", name="data_collection_group"):
         try:
             for hook in dcg_hooks:
