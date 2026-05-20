ff3a3abe1dbde71cd1c392a29a9bf4b4a8642cc2
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2026-05-20 18:32:41 +0100

    Updated DB test to check that only one file was registered

--- tests/workflows/fib/test_register_atlas.py
+++ tests/workflows/fib/test_register_atlas.py
@@ -291,8 +291,10 @@ def test_run_with_db(
     visit_dir: Path,
     murfey_db_session: Session,
 ):
-    test_file = (
-        visit_dir / "maps/LayersData/Layer/Electron Snapshot/Electron Snapshot.tiff"
+    test_files = (
+        visit_dir / "maps/LayersData/Layer/Electron Snapshot/Electron Snapshot.tiff",
+        visit_dir
+        / "maps/LayersData/Layer/Electron Snapshot/Electron Snapshot (2).tiff",
     )
 
     # Add a test visit to the database
@@ -310,33 +312,45 @@ def test_run_with_db(
     murfey_db_session.commit()
 
     # Mock the metadata returned from the image file
-    mock_metadata = FIBAtlasMetadata(
-        visit_name=visit_name,
-        file=test_file,
-        voltage=2000,
-        shift_x=0,
-        shift_y=0,
-        len_x=0.003072,
-        len_y=0.002048,
-        pos_x=0.003,
-        pos_y=0.0003,
-        pos_z=0.01,
-        rotation=-1.309,
-        tilt_alpha=0.8,
-        tilt_beta=0,
-        pixels_x=3072,
-        pixels_y=2048,
-        pixel_size_x=1e-6,
-        pixel_size_y=1e-6,
-    )
+    mock_metadata = [
+        FIBAtlasMetadata(
+            visit_name=visit_name,
+            file=test_file,
+            voltage=2000,
+            shift_x=0,
+            shift_y=0,
+            len_x=0.003072,
+            len_y=0.002048,
+            pos_x=0.003,
+            pos_y=0.0003,
+            pos_z=0.01,
+            rotation=-1.309,
+            tilt_alpha=0.8,
+            tilt_beta=0,
+            pixels_x=3072,
+            pixels_y=2048,
+            pixel_size_x=1e-6,
+            pixel_size_y=1e-6,
+        )
+        for test_file in test_files
+    ]
     mocker.patch(
         "murfey.workflows.fib.register_atlas._parse_metadata",
-        return_value=mock_metadata,
+        side_effect=mock_metadata,
     )
 
     # Run the function and check that it's run through to completion
-    assert run(
-        session_id=session_id,
-        file=test_file,
-        murfey_db=murfey_db_session,
-    )
+    for test_file in test_files:
+        run(
+            session_id=session_id,
+            file=test_file,
+            murfey_db=murfey_db_session,
+        )
+    search_results = murfey_db_session.exec(
+        select(MurfeyDB.ImagingSite).where(
+            MurfeyDB.ImagingSite.session_id == session_id
+        )
+    ).all()
+
+    assert len(search_results) == 1
+    assert search_results[0].image_path == str(mock_metadata[-1].file)
