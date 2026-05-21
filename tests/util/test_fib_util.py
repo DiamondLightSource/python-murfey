cb0a651a7ef31689b8a53dd07b49970152f2a2d2
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2026-05-21 09:59:10 +0100

    Migrated 'number_from_name' test to its own test module

---
+++ tests/util/test_fib_util.py
@@ -0,0 +1,21 @@
+import pytest
+
+from murfey.util.fib import number_from_name
+
+
+@pytest.mark.parametrize(
+    "test_params",
+    (  # File name | Expected number
+        # AutoTEM examples
+        ("Lamella", 1),
+        ("Lamella (2)", 2),
+        ("Lamella (12)", 12),
+        # Maps examples
+        ("Electron Snapshot", 1),
+        ("Electron Snapshot (3)", 3),
+        ("Electron Snapshot (21)", 21),
+    ),
+)
+def test_number_from_name(test_params: tuple[str, int]):
+    name, number = test_params
+    assert number_from_name(name) == number
