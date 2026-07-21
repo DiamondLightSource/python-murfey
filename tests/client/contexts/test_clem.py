4e7f54d39c1b1782778b354bbc67d5bb805eaf86
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2026-07-21 08:27:39 +0100

    Added placeholder tests for CLEMContext functions

---
+++ tests/client/contexts/test_clem.py
@@ -0,0 +1,14 @@
+def test_post_transfer_lif_data():
+    pass
+
+
+def test_post_transfer_tiff_data():
+    pass
+
+
+def test_process_lif_file():
+    pass
+
+
+def test_process_tiff_file():
+    pass
