5de7ac3daf01818682ea79e8161622f6f33a1080
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2026-08-19 11:40:24 +0100

    Removed redundant 'align_and_merge' API endpoint

--- src/murfey/server/api/workflow_clem.py
+++ src/murfey/server/api/workflow_clem.py
@@ -1,17 +1,11 @@
 from __future__ import annotations
 
 import json
-from ast import literal_eval
-from importlib.metadata import (
-    EntryPoint,  # type hinting only
-    entry_points,
-)
 from logging import getLogger
 from pathlib import Path
-from typing import Literal, Optional
 
 from fastapi import APIRouter
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel
 from sqlmodel import Session, select
 
 import murfey.server
@@ -185,72 +179,5 @@ def process_raw_tiffs(
         queue="processing_recipe",
         message=recipe,
         new_connection=True,
-    )
-    return True
-
-
-class AlignAndMergeParams(BaseModel):
-    # Processing parameters
-    series_name: str
-    images: list[Path]
-    metadata: Path
-    # Optional processing parameters
-    crop_to_n_frames: Optional[int] = None
-    align_self: Literal["enabled", ""] = ""
-    flatten: Literal["mean", "min", "max", ""] = ""
-    align_across: Literal["enabled", ""] = ""
-
-    @field_validator("images", mode="before")
-    @classmethod
-    def parse_stringified_list(cls, value):
-        if isinstance(value, str):
-            try:
-                eval_result = literal_eval(value)
-                if isinstance(eval_result, list):
-                    parent_tiffs = [Path(p) for p in eval_result]
-                    return parent_tiffs
-            except (SyntaxError, ValueError):
-                raise ValueError("Unable to parse input")
-        # Return value as-is; if it fails, it fails
-        return value
-
-
-@router.post("/sessions/{session_id}/align_and_merge_stacks")
-def align_and_merge_stacks(
-    session_id: int,
-    align_and_merge_params: AlignAndMergeParams,
-    db: Session = murfey_db,
-):
-    try:
-        # Try and load relevant Murfey workflow
-        workflow: EntryPoint = list(
-            entry_points(group="murfey.workflows", name="clem.align_and_merge")
-        )[0]
-    except IndexError:
-        raise RuntimeError("The relevant Murfey workflow was not found")
-
-    # Get instrument name from the database to load the correct config file
-    session_row: MurfeyDB.Session = db.exec(
-        select(MurfeyDB.Session).where(MurfeyDB.Session.id == session_id)
-    ).one()
-    instrument_name = session_row.instrument_name
-
-    # Pass arguments to correct workflow
-    workflow.load()(
-        # Match the arguments found in murfey.workflows.clem.align_and_merge
-        # Session parameters
-        session_id=session_id,
-        instrument_name=instrument_name,
-        # Processing parameters
-        series_name=align_and_merge_params.series_name,
-        images=align_and_merge_params.images,
-        metadata=align_and_merge_params.metadata,
-        # Optional processing parameters
-        crop_to_n_frames=align_and_merge_params.crop_to_n_frames,
-        align_self=align_and_merge_params.align_self,
-        flatten=align_and_merge_params.flatten,
-        align_across=align_and_merge_params.align_across,
-        # Optional session parameters
-        messenger=murfey.server._transport_object,
     )
     return True
