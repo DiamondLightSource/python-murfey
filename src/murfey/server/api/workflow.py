4b78c308861f1ffa34dc05329042fc14d7a87c8f
Stephen Riggs <122790971+stephen-riggs@users.noreply.github.com>
2026-08-28 12:29:08 +0100

    Skip Scaup calls if sample was previously known (#875)
    The atlas update hook gets requested many times which means we repeatedly load the scaup hook and post an updated sample. There is no point doing this if the sample hasn't changed.

--- src/murfey/server/api/workflow.py
+++ src/murfey/server/api/workflow.py
@@ -187,6 +187,16 @@ def register_dc_group(
         # Or registering a new atlas-type dcg for a sample that is already present
         for dcg_instance in dcg_murfey:
             # Update all instances in case there are multiple processing runs
+            # Skip Scaup registration if sample is unchanged
+            sample_registered = (
+                dcg_instance.sample is not None
+                and dcg_params.sample
+                in [
+                    None,
+                    dcg_instance.sample,
+                ]
+            )
+            # Update dcg table
             dcg_instance.atlas = dcg_params.atlas or dcg_instance.atlas
             dcg_instance.sample = dcg_params.sample or dcg_instance.sample
             dcg_instance.atlas_pixel_size = (
@@ -239,6 +249,7 @@ def register_dc_group(
                             "atlas_id": dcg_instance.atlas_id,
                             "atlas": dcg_params.atlas,
                             "sample": dcg_params.sample,
+                            "sample_registered": sample_registered,
                             "atlas_pixel_size": dcg_params.atlas_pixel_size,
                             "dcgid": dcg_instance.id,
                             "session_id": session_id,
