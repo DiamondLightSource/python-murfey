8e9d10d63caa4215e30596f39299e554c07a5c5e
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2026-09-11 16:44:44 +0100

    test fixes

--- tests/client/contexts/test_atlas.py
+++ tests/client/contexts/test_atlas.py
@@ -100,6 +100,7 @@ def test_atlas_context_dm(mock_capture_post, tmp_path):
     )
 
     # Write sample dm file
+    (tmp_path / "cm12345-6/Supervisor_atlas/Sample2/Atlas/Atlas_01.mrc").touch()
     atlas_dm = tmp_path / "cm12345-6/Supervisor_atlas/Sample2/Atlas/Atlas.dm"
     atlas_dm.parent.mkdir(parents=True)
     (tmp_path / "cm12345-6/Supervisor_atlas/Sample2/Atlas/Atlas_01.mrc").touch()
