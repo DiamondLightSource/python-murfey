3f508619c3509e96513355934b31a047fb633b3b
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2026-09-15 11:39:00 +0100

    Fixed broken tests

--- tests/server/test_feedback.py
+++ tests/server/test_feedback.py
@@ -4,6 +4,8 @@ from unittest.mock import MagicMock
 import pytest
 from pytest_mock import MockerFixture
 
+from murfey.server.feedback import feedback_callback
+
 
 @pytest.mark.parametrize(
     "entry_point_name",
@@ -31,26 +33,20 @@ def test_feedback_callback(
     Checks that feedback-callback loop works correctly for the entry points-based workflows
     """
     # Patch the functions used to generate the module-level variables
-    mock_get_security_config = mocker.patch("murfey.util.config.get_security_config")
-    mock_get_security_config.return_value = MagicMock()
-    mock_url = mocker.patch("murfey.server.murfey_db.url")
-    mock_url.return_value = MagicMock()
-    mock_create_engine = mocker.patch("sqlmodel.create_engine")
-    mock_create_engine.return_value = MagicMock()
+    mocker.patch("murfey.server.feedback.get_security_config", return_value=MagicMock())
+    mocker.patch("murfey.server.feedback.url", return_value=MagicMock())
+    mocker.patch("murfey.server.feedback.create_engine", return_value=MagicMock())
     mock_murfey_db = MagicMock()
-    mock_sql_session = mocker.patch("sqlmodel.Session")
-    mock_sql_session.return_value = mock_murfey_db
+    mock_sql_session = mocker.patch("murfey.server.feedback.Session")
+    mock_sql_session.return_value.__enter__.return_value = mock_murfey_db
 
     # Load the entry point and patch the executable it calls
     eps = list(entry_points(group="murfey.workflows", name=entry_point_name))
     assert len(eps) == 1  # Entry point should be present and unique
     mock_function = mocker.patch(eps[0].value.replace(":", "."))
 
-    # Initialise after mocking
-    from murfey.server.feedback import feedback_callback
-
     # Run the function and check that it calls the entry point correctly
     header = {"dummy": "dummy"}
     message = {"register": entry_point_name}
-    feedback_callback(header, message, mock_murfey_db)
+    feedback_callback(header, message)
     mock_function.assert_called_once_with(message=message, murfey_db=mock_murfey_db)
