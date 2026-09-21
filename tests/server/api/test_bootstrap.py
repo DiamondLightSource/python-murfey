7e87a6ccffcfaf23c5b48b99d40604ece06f03f4
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2026-09-21 18:51:52 +0100

    Added test for 'get_plugin_wheel'

--- tests/server/api/test_bootstrap.py
+++ tests/server/api/test_bootstrap.py
@@ -69,3 +69,40 @@ def test_show_plugin_wheels(
     """
     # Check that it was constructed correctly
     assert response.content.decode() == html_page
+
+
+@pytest.mark.parametrize("package_found", (True, False))
+def test_get_plugin_wheel(
+    mocker: MockerFixture,
+    package_found: bool,
+    tmp_path: Path,
+):
+    # Set up test parameters
+    instrument_name = "murfey-test"
+    package_name = "package_a"
+
+    # Create a test file
+    test_package = tmp_path / package_name
+    test_package.touch(exist_ok=True)
+
+    # Mock the 'get_machine_config' return value
+    plugin_packages = {"package_a": test_package}
+    config = MachineConfig(plugin_packages=plugin_packages)
+    mock_get_machine_config = mocker.patch(
+        "murfey.server.api.bootstrap.get_machine_config",
+        return_value={instrument_name: config},
+    )
+
+    # Set up the test backend client and the URL to poke
+    backend_server = set_up_test_backend_client()
+    backend_url_path = url_path_for(
+        "api.bootstrap.plugins",
+        "get_plugin_wheel",
+        instrument_name=instrument_name,
+        package="package_a" if package_found else "package_b",
+    )
+
+    # Poke it and check that the calls and response are as expected
+    response = backend_server.get(backend_url_path)
+    mock_get_machine_config.assert_called_once_with(instrument_name=instrument_name)
+    assert response.status_code == 200 if package_found else 404
